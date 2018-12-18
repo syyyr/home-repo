c7d5eb11d1646b2f449204e718e5edee365359f3
Václav Kubernát <kubervac@fit.cvut.cz>
2018-12-18 19:05:37 +0100

    Upgrade lock.bash

--- apps/lock.bash
+++ apps/lock.bash
@@ -1,5 +1,9 @@
 #!/bin/bash
-i3lock -t -i ~/.local/share/chrono.png
-sleep 1
+#i3lock -t -i ~/.local/share/chrono.png
+#i3lock -t -i ~/.local/share/win10lock.png
 kbacklight 0
-xset dpms force off
+TIME="$(date '+%H:%M')"
+DATE="$(date '+%A, %d. %B')"
+convert .local/share/win10lock-template.png -font .local/share/fonts/segoeuil.ttf -fill white -pointsize 127 -draw "text 40,893 '$TIME'" -pointsize 57 -draw "text 40,970 '$DATE'" /tmp/lockscreen.png
+i3lock -t -i /tmp/lockscreen.png
+#xset dpms force off
