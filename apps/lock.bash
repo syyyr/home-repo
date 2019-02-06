308da206fbea86a5275d50cd6df906e369c71116
Václav Kubernát <kubervac@fit.cvut.cz>
2019-02-06 12:36:30 +0100

    Make lock.bash mute sound

--- apps/lock.bash
+++ apps/lock.bash
@@ -2,8 +2,16 @@
 #i3lock -t -i ~/.local/share/chrono.png
 #i3lock -t -i ~/.local/share/win10lock.png
 kbacklight 0
+if ! volume silent | grep muted; then
+    volume silent toggle
+    TOGGLE=yes
+fi
 TIME="$(date '+%H:%M')"
 DATE="$(date '+%A, %d. %B')"
 convert .local/share/win10lock-template.png -font .local/share/fonts/segoeuil.ttf -fill white -pointsize 127 -draw "text 40,893 '$TIME'" -pointsize 57 -draw "text 40,970 '$DATE'" /tmp/lockscreen.png
-i3lock -t -i /tmp/lockscreen.png
+i3lock -n -t -i /tmp/lockscreen.png
+
+if [ $TOGGLE = "yes" ]; then
+    volume silent toggle
+fi
 #xset dpms force off
