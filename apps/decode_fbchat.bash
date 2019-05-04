de86fc142f1a9a89126718c50422e485215ad3d7
Václav Kubernát <kubervac@fit.cvut.cz>
2019-05-04 11:14:38 +0200

    Fix decode_fbchat.bash
    - don't rely on ftfy for unicode
    - filter even lines

--- apps/decode_fbchat.bash
+++ apps/decode_fbchat.bash
@@ -2,16 +2,17 @@
 if [ $# != 1 ]; then
     exit 1
 fi
-PARSED_JSON="$(jq '.messages | map(.timestamp_ms,.sender_name,.content)[]' $1)"
+FIXED_ESCAPE="$(sed 's/\\u00/\\x/g' $1)"
+FIXED_UNICODE="$(echo -e "$FIXED_ESCAPE")"
+PARSED_JSON="$(jq '.messages | map(.timestamp_ms,.sender_name,.content)[]' <<< $FIXED_UNICODE)"
 echo "parsed messages.json" >&2
-FIXED_UNICODE="$(ftfy <<< "$PARSED_JSON")"
-echo "unicode fixed" >&2
+echo "$PARSED_JSON" >&2
 
-TIMESTAMPS="$(awk 'NR % 3 == 1' <<< $FIXED_UNICODE)"
+TIMESTAMPS="$(awk 'NR % 3 == 1' <<< $PARSED_JSON)"
 echo "timestamps done" >&2
-SENDERS="$(awk 'NR % 3 == 2' <<< $FIXED_UNICODE)"
+SENDERS="$(awk 'NR % 3 == 2' <<< $PARSED_JSON)"
 echo "senders done" >&2
-MESSAGES="$(awk 'NR % 3 == 0' <<< $FIXED_UNICODE)"
+MESSAGES="$(awk 'NR % 3 == 0' <<< $PARSED_JSON)"
 echo "messages done" >&2
 #echo "$TIMESTAMPS"
 #echo "$SENDERS"
@@ -30,6 +31,8 @@ SENDERS=$(sed 's/$/:/' <<< "$SENDERS")
 MESSAGES=$(sed -e 's/^"//' -e 's/"$//' <<< "$MESSAGES")
 
 echo final paste... >&2
-RES=$(paste -d' ' <(echo "$DATES") <(echo "$SENDERS") <(echo "$MESSAGES"))
+FINAL=$(paste -d' ' <(echo "$DATES") <(echo "$SENDERS") <(echo "$MESSAGES"))
+echo removing even lines... >&2
+RES=$(sed 'n; d' <<< "$FINAL")
 echo "$RES"
 echo done. >&2
