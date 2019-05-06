2a48a90855d9565d28dd0c2a080b5dab0064c3fd
Václav Kubernát <kubervac@fit.cvut.cz>
2019-05-07 01:02:04 +0200

    Swap workspaces a little in workspace_ball

--- apps/workspace_ball.bash
+++ apps/workspace_ball.bash
@@ -3,6 +3,6 @@ OUTPUT=$(xrandr | grep -o "^.* connected" | grep -v "eDP1" | sed 's/ connected//
 i3 "workspace 2; move workspace to output $OUTPUT"
 i3 "workspace 3; move workspace to output $OUTPUT"
 i3 "workspace 4; move workspace to output $OUTPUT"
-i3 "workspace M; move workspace to output eDP1"
 i3 "workspace N; move workspace to output eDP1"
+i3 "workspace M; move workspace to output eDP1"
 i3 "workspace 1; move workspace to output $OUTPUT"
