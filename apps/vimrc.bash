e6cfb71ae97da2d198521950eae2066065a48cbb
Václav Kubernát <kubervac@fit.cvut.cz>
2019-05-15 23:04:34 +0200

    Open less files in vimrc.bash by default

--- apps/vimrc.bash
+++ apps/vimrc.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ $# -eq 0 ]]; then
-    nvim -p ~/.config/nvim/*.vim ~/.config/nvim/ftplugin/*
+    nvim -p ~/.config/nvim/*.vim ~/.config/nvim/autoload/custom.vim
 else
     nvim -p $(printf '/home/vk/.config/nvim/%s ' "$@")
 fi
