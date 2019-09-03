38dfdd033a7f4095bd60ed08eaa1d67854671e09
Václav Kubernát <kubervac@fit.cvut.cz>
2019-09-03 13:28:34 +0200

    Improve lock.bash image generation
    It now works properly on my work monitors.

--- apps/lock.bash
+++ apps/lock.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
-#i3lock -t -i ~/.local/share/chrono.png
-#i3lock -t -i ~/.local/share/win10lock.png
+# TODO: Make this faster somehow. The initial lock takes forever.
 kbacklight 0
 TOGGLE=no
 if ! volume silent | grep muted; then
@@ -9,9 +8,16 @@ if ! volume silent | grep muted; then
 fi
 TIME="$(date '+%H:%M')"
 DATE="$(date '+%A, %d. %B')"
-convert $HOME/.local/share/win10lock-template.png -font $HOME/.local/share/fonts/segoeuil.ttf -fill white -pointsize 127 -draw "text 40,893 '$TIME'" -pointsize 57 -draw "text 40,970 '$DATE'" /tmp/lockscreen.png
+convert /home/vk/.local/share/win10lock-template-res.png \
+    -font $HOME/.local/share/fonts/segoeuil.ttf \
+    -fill white \
+    -pointsize 127 -draw "text 40,893 '$TIME'" \
+    -pointsize 57 -draw "text 40,970 '$DATE'" \
+    -pointsize 169 -draw "text 1973,1190 '$TIME'" \
+    -pointsize 76 -draw "text 1973,1293 '$DATE'" \
+    /tmp/lockscreen.png
 systemctl --user start lock_generator.service
-/usr/local/bin/i3lock -n -t -i /tmp/lockscreen.png
+/usr/local/bin/i3lock -n -i /tmp/lockscreen.png
 systemctl --user stop lock_generator.service
 
 if [ $TOGGLE = "yes" ]; then
