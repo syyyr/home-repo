4bc862f73bb02d3bed13c5de244f36d97dcc256d
Václav Kubernát <kubervac@fit.cvut.cz>
2019-10-28 12:20:21 +0100

    Add config for aurgit

--- .config/bashrc/env.bash
+++ .config/bashrc/env.bash
@@ -1,3 +1,4 @@
+export AURGIT_PAGER=""
 export DAYSTART="9"
 export EDITOR=nvim
 export FZF_DEFAULT_COMMAND='find . -type d \( -name ".cargo" -or -name ".local" -or -name ".rustup" -or -name ".npm" -or -name ".git" -or -name ".cache" \) -prune -o -type f -print'
