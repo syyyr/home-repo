fada147fb28aaac5188df3c4ab1ce4f62c13ca5e
Václav Kubernát <kubervac@fit.cvut.cz>
2019-11-19 05:16:40 +0100

    Remove first argument of git_cmp
    I never really used this, so HEAD is used.

--- .config/bashrc/functions.bash
+++ .config/bashrc/functions.bash
@@ -32,7 +32,7 @@ res()
 
 git_cmp()
 {
-    nvim -d "$2" <(git show "$1":"$2") -c "let g:git_cmp_ft = &ft | windo let &ft = g:git_cmp_ft"
+    nvim -d "$1" <(git show HEAD:"$1") -c "let g:git_cmp_ft = &ft | windo let &ft = g:git_cmp_ft"
 }
 
 if [[ -f /usr/share/nvm/init-nvm.sh ]]; then
