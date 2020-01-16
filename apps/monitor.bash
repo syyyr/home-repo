304d041a59b571e0fb6ca0ab78da2cae6ee82e25
Václav Kubernát <kubervac@fit.cvut.cz>
2020-01-16 19:16:11 +0100

    Wait before monitor.bash starts
    I got tired of disconnected the monitor while rebooting, so this fixes
    it.

--- apps/monitor.bash
+++ apps/monitor.bash
@@ -1,14 +1,7 @@
 #!/bin/bash
 export DISPLAY=:0.0
 
-
-udevadm monitor -k | while IFS= read -r line;
-do
-
-    if [ -z "$(grep -F "/devices/pci0000:00/0000:00:02.0/drm/card0 (drm)" <<< $line)" ]; then
-        continue
-    fi
-
+refresh() {
     OUTPUT=$(xrandr | grep -o "^.* connected" | grep -v "eDP1" | sed 's/ connected//')
     echo $OUTPUT
     if [ -z $OUTPUT ]; then
@@ -20,4 +13,17 @@ do
         $HOME/apps/workspace_ball.bash
         nitrogen --restore
     fi
+}
+
+sleep 3 # Wait a bit. If we just booted, this script wouldn't work right away.
+refresh
+
+udevadm monitor -k | while IFS= read -r line;
+do
+
+    if [ -z "$(grep -F "/devices/pci0000:00/0000:00:02.0/drm/card0 (drm)" <<< $line)" ]; then
+        continue
+    fi
+
+    refresh
 done
