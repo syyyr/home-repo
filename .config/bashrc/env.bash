24c87468b45ce078a9a4fe0f422fa3bdad1098fb
Václav Kubernát <sir.venceslas@gmail.com>
2020-03-11 09:29:23 +0100

    Remove env variable

--- .config/bashrc/env.bash
+++ .config/bashrc/env.bash
@@ -1,4 +1,3 @@
-export AURGIT_PAGER=""
 export DAYSTART="9"
 export EDITOR="nvim"
 export FZF_DEFAULT_COMMAND='find . -type d \( -name ".cargo" -or -name ".local" -or -name ".rustup" -or -name ".npm" -or -name ".git" -or -name ".cache" \) -prune -o -type f -print'
