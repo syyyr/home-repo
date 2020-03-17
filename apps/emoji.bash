1371fbcce0769be554fb977a4b55e3436af75d9f
Václav Kubernát <sir.venceslas@gmail.com>
2020-03-17 13:04:19 +0100

    Improve emoji picker
    As the man page, suggest, run it on key release. Also, the clipboard
    doesn't get overriden if nothing is picked.

--- apps/emoji.bash
+++ apps/emoji.bash
@@ -9,10 +9,13 @@ angery 😠
 rofl 🤣
 heart ❤
 heart_eyes 😍
+hundred/100 💯
 b 🅱️'
 
 SELECTED_EMOJI="$(rofi -dmenu -matching fuzzy -p "> " <<< "$EMOJI")"
 RES="$(sed -r 's/^[^ ]+ //' <<< "$SELECTED_EMOJI")"
 
 # I have no idea why this doesn't work without nohup, when this script gets run by i3, but OK
-nohup xclip -rmlastnl -se c <<< "$RES" &> /dev/null
+if [[ -n "$RES" ]]; then
+    nohup xclip -rmlastnl -se c <<< "$RES" &> /dev/null
+fi
