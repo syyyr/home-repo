c7da5a0f3a7e6788e574646139c6449ec7589e3f
Václav Kubernát <sir.venceslas@gmail.com>
2020-09-08 18:43:50 +0200

    Rework area-gif to use escrotum
    Although the previous solution worked, I'd much rather use something
    more user-friendly than ffcast/ffmpeg combo.

--- apps/area-gif.bash
+++ apps/area-gif.bash
@@ -1,7 +1,12 @@
 #!/bin/bash
+set -e
+
+exec &> ~/log.out
 notify-send -t 3000 -R /tmp/area-gif-not-id "Select area..."
-ffcast -s % ffmpeg -y -f x11grab -show_region 1 -framerate 15 -video_size %s -i %D+%c -codec:v huffyuv -vf crop="iw-mod(iw\\,2):ih-mod(ih\\,2)" /tmp/area-gif.avi
+escrotum -rs /tmp/area-gif.webm
 notify-send -t 100000 -R /tmp/area-gif-not-id "Converting to gif..."
+ffmpeg -i /tmp/area-gif.webm /tmp/area-gif.avi
+rm /tmp/area-gif.webm
 convert -set delay 10 -layers Optimize /tmp/area-gif.avi /tmp/area-gif.gif
-notify-send -t 3000 -R /tmp/area-gif-not-id "Saved as /tmp/area-gif.gif"
 rm /tmp/area-gif.avi
+notify-send -t 3000 -R /tmp/area-gif-not-id "Saved as /tmp/area-gif.gif"
