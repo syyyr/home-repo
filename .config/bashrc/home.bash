361b77603a205ba28b1f6760655c9078cd836992
Václav Kubernát <sir.venceslas@gmail.com>
2020-09-09 16:45:22 +0200

    Cleanup home.bash

--- .config/bashrc/home.bash
+++ .config/bashrc/home.bash
@@ -2,27 +2,25 @@
 # see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
 # for examples
 
-umask 022
-
-export BROWSER=wslbrowser
+export BROWSER="wslbrowser"
 
 #locale
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
+export LC_ALL="en_US.UTF-8"
+export LANG="en_US.UTF-8"
 
-export DISPLAY=localhost:0
+export DISPLAY="localhost:0"
 
 #win cmd autocomplete BS
-source $HOME/.rustup-comp
+source "$HOME/.rustup-comp"
 
 #screen BS
-export SCREENDIR=$HOME/.screen
+export SCREENDIR="$HOME/.screen"
 
 #x server BS
 if [ -z "$SSH_CLIENT" ]; then
-export DISPLAY=localhost:0
-export RUNLEVEL=3
-rm -rf .cache/sessions
+export DISPLAY="localhost:0"
+export RUNLEVEL="3"
+rm -rf "$HOME/.cache/sessions"
 fi
 xmodmap -e 'keycode 126='
 
