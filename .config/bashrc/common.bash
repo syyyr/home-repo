7bae7b6f1714d068f611d9e8eb3e8e873d7d0058
Václav Kubernát <sir.venceslas@gmail.com>
2020-09-09 16:24:23 +0200

    bashrc: Always go for color prompt
    I don't want a no color terminal lol.

--- .config/bashrc/common.bash
+++ .config/bashrc/common.bash
@@ -20,27 +20,6 @@ HISTFILESIZE='50000'
 # update the values of LINES and COLUMNS.
 shopt -s checkwinsize
 
-# set a fancy prompt (non-color, unless we know we "want" color)
-case "$TERM" in
-    xterm-color|*-256color) color_prompt='yes';;
-esac
-
-# uncomment for a colored prompt, if the terminal has the capability; turned
-# off by default to not distract the user: the focus in a terminal window
-# should be on the output of commands, not on the prompt
-#force_color_prompt=yes
-
-if [ -n "$force_color_prompt" ]; then
-    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
-	# We have color support; assume it's compliant with Ecma-48
-	# (ISO/IEC-6429). (Lack of such support is extremely rare, and such
-	# a case would tend to support setf rather than setaf.)
-	color_prompt=yes
-    else
-	color_prompt=
-    fi
-fi
-
 _STATUS()
 {
     local GOOD=(👍 😂 👌)
@@ -53,16 +32,11 @@ _STATUS()
     fi
 }
 
-if [ "$color_prompt" = yes ]; then
-    PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] $(_STATUS $?)\n$ '
-else
-    PS1='\u@\h:\w\$ '
-fi
+PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] $(_STATUS $?)\n$ '
 
 if [ -n "$SSH_CLIENT" ]; then
     PS1='\[\033[38;5;83m\]\u@\h\[\033[00m\]:\[\033[38;5;63m\]\w\[\033[00m\]\n\$ '
 fi
-unset color_prompt force_color_prompt
 
 # If this is an xterm set the title to user@host:dir
 case "$TERM" in
