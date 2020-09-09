522a69b09662489e97bc8b9fc7e404ad6dae61a1
Václav Kubernát <sir.venceslas@gmail.com>
2020-09-09 16:16:22 +0200

    Remove manual sourcing of bash_completion
    This should be handled by some sort of a global bashrc.

--- .config/bashrc/common.bash
+++ .config/bashrc/common.bash
@@ -82,15 +82,6 @@ if [ -x /usr/bin/dircolors ]; then
     alias egrep='egrep --color=auto'
 fi
 
-if ! shopt -oq posix; then
-  if [ -f /usr/share/bash-completion/bash_completion ]; then
-    . /usr/share/bash-completion/bash_completion
-  elif [ -f /etc/bash_completion ]; then
-    . /etc/bash_completion
-  fi
-fi
-
-
 bind '"\e[18~":""'
 stty susp undef
 stty -ixon
