ea6f75f648e58599fa520f18bdf3b5cbca2a086e
Václav Kubernát <sir.venceslas@gmail.com>
2020-09-11 01:10:50 +0200

    Move lockscrenn date and fix bug
    The background didn't have a proper path.

--- apps/lock.bash
+++ apps/lock.bash
@@ -4,18 +4,17 @@
 if [[ "$(i3lock -v 2>&1)" =~ [0-9]+\.[0-9]+\.c\.[0-9]+ ]]; then
     ARGS='
     -k
-    -i .local/share/win10lock-template.png
+    -i '$HOME'/.local/share/win10lock-template.png
     --time-font=Segoeui
     --date-font=Segoeui
     --timesize=130
-    --date-align=left
     --datesize=59
     --timestr="%H:%M"
     --datestr="%A, %d. %B"
     --datecolor=FFFFFF
     --timecolor=FFFFFF
     --timepos=200:893
-    --datepos="tx+55:ty+80"'
+    --datepos="240:ty+80"'
 fi
 
 $HOME/bin/kbacklight 0
