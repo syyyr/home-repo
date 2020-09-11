0d6d00ce91a6efbe58d98b3b8deb23c98d383389
Václav Kubernát <sir.venceslas@gmail.com>
2020-09-11 10:18:22 +0200

    Add keybind for copypasta settings

--- apps/copypasta.bash
+++ apps/copypasta.bash
@@ -2,7 +2,7 @@
 PASTAS='thinking 🤔
 tears 😭
 joy 😂
-:DD 😁
+:DD/grin 😁
 ok_hand 👌
 :D 😀
 :* 😘
@@ -20,6 +20,7 @@ devil 😈
 shrug ¯\_(ツ)_/¯
 tm ™
 green_circle 🟢
+sushi 🍣
 linux/interject I'\''d just like to interject for a moment. What you'\''re referring to as Linux, is in fact, GNU/Linux, or as I'\''ve recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX.
 b 🅱️'
 
