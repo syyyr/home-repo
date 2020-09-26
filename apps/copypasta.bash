282b340dfc8288e62657b1a2526b79b641b2bffc
Václav Kubernát <sir.venceslas@gmail.com>
2020-09-26 09:42:02 +0200

    Sort stuff in copypasta.bash

--- apps/copypasta.bash
+++ apps/copypasta.bash
@@ -18,6 +18,7 @@ roll 🙄
 sweat 😅
 hundred/100 💯
 fear/scared 😱
+honk/goose 🦢
 devil 😈
 shrug ¯\_(ツ)_/¯
 tm ™
@@ -26,7 +27,7 @@ sushi 🍣
 linux/interject I'\''d just like to interject for a moment. What you'\''re referring to as Linux, is in fact, GNU/Linux, or as I'\''ve recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX.
 b 🅱️'
 
-SELECTED_PASTA="$(rofi -dmenu -matching fuzzy -p "> " <<< "$PASTAS")"
+SELECTED_PASTA="$(rofi -dmenu -sort -matching fuzzy -p "> " <<< "$PASTAS")"
 RES="$(sed -r 's/^[^ ]+ //' <<< "$SELECTED_PASTA")"
 
 # I have no idea why this doesn't work without nohup, when this script gets run by i3, but OK
