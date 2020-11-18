784320503f2587b3f4fe05dd179307e782848b5f
Václav Kubernát <sir.venceslas@gmail.com>
2020-11-18 17:53:19 +0100

    battery: Play notification sound on battery low

--- apps/battery.bash
+++ apps/battery.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 THRESHOLD=10
+export DISPLAY=":0"
+"$HOME/apps/check_available.bash" ffplay || exit 1
 
 if ! (acpi -a | grep "off-line" > /dev/null); then
     exit 0
@@ -11,5 +13,6 @@ if [[ "$BATTERY_LEVEL" -le "$THRESHOLD" ]]; then
     ARGS='-t 5000 -h string:x-canonical-private-synchronous:kbacklight'
     ARGS+=" -i $KEYBOARD_IMG"
     notify-send $ARGS 'Battery low' "$BATTERY_LEVEL%"
+    ffplay ~/.local/share/notification.mp3 -nodisp -autoexit
 fi
 
