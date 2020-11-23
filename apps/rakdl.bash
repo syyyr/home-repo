46d6a79ee642b802dc9b25f174112fd2fe4cab54
Václav Kubernát <sir.venceslas@gmail.com>
2020-11-23 17:20:24 +0100

    Add rakdl.bash
    Counterpart to rakup.bash allowing easier download of files backed to
    rakac.

---
+++ apps/rakdl.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+"$HOME/apps/check_available.bash" fzf
+
+# Set TERM and use dircolors to tell remote bash that I'm using a color enabled terminal
+FILE=$(ssh "rak@anip.icu" 'export TERM=xterm-256color; eval $(dircolors); ls -A --color=always www/rakac' | fzf -0 --height=50% --border --ansi);
+
+scp "rak@anip.icu:www/rakac/$FILE" .
