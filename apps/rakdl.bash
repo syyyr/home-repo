5a57206909b7a8cdf496188594c46751cf96408c
Václav Kubernát <sir.venceslas@gmail.com>
2020-11-24 03:03:01 +0100

    rakdl: Fail the script if fzf fails

--- apps/rakdl.bash
+++ apps/rakdl.bash
@@ -3,5 +3,8 @@
 
 # Set TERM and use dircolors to tell remote bash that I'm using a color enabled terminal
 FILE=$(ssh "rak@anip.icu" 'export TERM=xterm-256color; eval $(dircolors); ls -A --color=always www/rakac' | fzf -0 --height=50% --border --ansi);
+if [[ "$?" -ne 0 ]]; then
+    exit 1
+fi
 
 scp "rak@anip.icu:www/rakac/$FILE" .
