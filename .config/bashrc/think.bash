72ba5589633b5b120aba1b4090525ec52c9e295c
Václav Kubernát <sir.venceslas@gmail.com>
2021-01-13 02:43:01 +0100

    Add cesnet stuff to PATH

--- .config/bashrc/think.bash
+++ .config/bashrc/think.bash
@@ -1,6 +1,9 @@
 export PATH="$HOME/bin:$PATH"
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
+export PATH="/opt/cesnet/bin:$PATH"
+export PATH="/opt/cesnet-au/bin:$PATH"
+export PATH="/opt/cesnet-t/bin:$PATH"
 
 export SCREENDIR="$HOME/.screen"
 
