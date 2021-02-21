69f3e6221cc054411ee777b91633ded05420bbc0
Václav Kubernát <sir.venceslas@gmail.com>
2021-02-21 22:42:22 +0100

    vimrc,bashrc: Rework completion
    Finally uses fzf!

--- .config/bashrc/completerc.bash
+++ .config/bashrc/completerc.bash
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-_vimrc_complete() {
-    # TODO: This has to be improved to work with after/. If I type ftplugin/, I don't want the after/ directory.
-    # Maybe I'll use fzf for this.
-    COMPREPLY+=($( cd "$HOME/.config/nvim"; find -not -name '\.*' -type f | sed s@\./@@ | grep -F "$2" ))
+__impl_path_compl() {
+    command find -not -name '\.*' -type f | sed s@\./@@
 }
 
-_bashrc_complete() {
-    COMPREPLY+=($( cd "$HOME/.config/bashrc"; find -not -name '\.*' -type f | sed s@\./@@ | grep -F "$2"))
+_setup_path_compl() {
+    eval "_$1_complete() {
+        cd '$2'
+        FZF_COMPLETION_TRIGGER="" __fzf_generic_path_completion __impl_path_compl '-m' ''
+    }"
+    complete -F "_$1_complete" "$1"
 }
 
 complete -W 'increase decrease min max'  brightness
 complete -W 'increase decrease toggle' volume
 complete -W 'toggle manual timeout' kbacklight_ctl
 complete -C "$HOME/bin/tasklist" taskkill
-complete -F _vimrc_complete vimrc
-complete -F _bashrc_complete bashrc
+_setup_path_compl "vimrc" "$HOME/.config/nvim"
+_setup_path_compl "bashrc" "$HOME/.config/bashrc"
 complete -C "$HOME/apps/twitch_online.bash" twitch
