e089be3c2d1de6531608fde3dee69c8e213ac74f
Václav Kubernát <sir.venceslas@gmail.com>
2021-03-20 11:29:19 +0100

    Add completions for `try`

--- .config/bashrc/completerc.bash
+++ .config/bashrc/completerc.bash
@@ -13,6 +13,32 @@ _setup_path_compl() {
     complete -F "_$1_complete" "$1"
 }
 
+__do_completion_for() {
+    # Some completions are loaded dynamically on demand
+    _completion_loader "$1"
+    completion=$(complete -p "$1" 2>/dev/null | awk '{print $(NF-1)}')
+
+    if [[ -n "$completion" ]]; then
+        "$completion"
+    fi
+}
+
+__try_compl() {
+    local completion
+
+    COMP_LINE=("${COMP_LINE:4}")
+    COMP_POINT=$(($COMP_POINT - 4))
+    COMP_WORDS=("${COMP_WORDS[@]:1}")
+    COMP_CWORD=$(($COMP_CWORD - 1))
+
+    if [[ "${#COMP_WORDS[@]}" -eq 1 ]]; then
+        COMPREPLY=( $(compgen -c "${COMP_WORDS}" | sort -u) )
+        return
+    fi
+
+    __do_completion_for "${COMP_WORDS[0]}"
+}
+
 complete -W 'increase decrease min max'  brightness
 complete -W 'increase decrease toggle' volume
 complete -W 'toggle manual timeout' kbacklight_ctl
@@ -20,3 +46,4 @@ complete -C "$HOME/bin/tasklist" taskkill
 _setup_path_compl "vimrc" "$HOME/.config/nvim"
 _setup_path_compl "bashrc" "$HOME/.config/bashrc"
 complete -C "$HOME/apps/twitch_online.bash" twitch
+complete -F __try_compl try
