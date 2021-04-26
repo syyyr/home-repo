194aefc337a0affdb67f8422b503e93ccc60d01e
Václav Kubernát <sir.venceslas@gmail.com>
2021-04-26 02:43:50 +0200

    prompt: Show command duration

--- .config/bashrc/common.bash
+++ .config/bashrc/common.bash
@@ -37,22 +37,39 @@ _GEN_PROMPT()
 {
     # ERROR has to be generated first, otherwise $? gets overwritten
     local ERROR="$(_PROMPT_ERROR "$?")"
-    local GREEN_BOLD=$'\033''[01;32m' BLUE_BOLD=$'\033''[01;34m' CURSIVE_GRAY=$'\033''[00;38;5;7;3m' NORMAL_COLOR=$'\033''[00m'
+    if [[ "$_COMMAND_START_TIME" ]]; then
+        local CURRENT_TIME="$(date '+%s%3N' | tr -d '\n')"
+        # If the execution time is less than 1 second, don't bother showing the execution time. It won't be too precise anyway.
+        if [[ $(("$CURRENT_TIME" - "$_COMMAND_START_TIME")) -ge 1000 ]]; then
+            local LAST_COMMAND_DURATION=" ($(bc <<< "scale=3; $(("$CURRENT_TIME" - "$_COMMAND_START_TIME")) / 1000")s)"
+        fi
+        unset _COMMAND_START_TIME
+    fi
+    local TITLE=$'\033'']0;' GREEN_BOLD=$'\033''[01;32m' BLUE_BOLD=$'\033''[01;34m' CURSIVE_GRAY=$'\033''[00;38;5;7;3m' NORMAL_COLOR=$'\033''[00m'
     local USER_HOST="$(expand_prompt '\u@\h')"
     local WORKDIR="$(expand_prompt '\w')"
     local PROMPT_COLORLESS="${USER_HOST}:${WORKDIR}${ERROR}"
     local TIME="$(date "+%H:%M:%S" | tr -d '\n')"
     # Eight characters for the date.
-    local NUM_SPACES="$((${COLUMNS} - ${#PROMPT_COLORLESS} - 8))"
+    local NUM_SPACES="$((${COLUMNS} - ${#PROMPT_COLORLESS} - ${#LAST_COMMAND_DURATION} - 8))"
     if [[ "$ERROR" ]]; then
         # Emojis actually take up two columns, but bash counts them as 1 character.
         NUM_SPACES="$(("$NUM_SPACES" - 1))"
     fi
     local SPACES="$(printf ' %.0s' $(seq 1 "${NUM_SPACES}"))"
-    PS1="${GREEN_BOLD}${USER_HOST}${NORMAL_COLOR}:${BLUE_BOLD}${WORKDIR}${CURSIVE_GRAY}${ERROR}${SPACES}${TIME}${NORMAL_COLOR}"'\n$ '
+    PS1="${GREEN_BOLD}${USER_HOST}${NORMAL_COLOR}:${BLUE_BOLD}${WORKDIR}\\a${CURSIVE_GRAY}${ERROR}${SPACES}${TIME}${LAST_COMMAND_DURATION}${NORMAL_COLOR}"'\n$ '
+}
+PROMPT_COMMAND=_GEN_PROMPT
+
+_SAVE_STARTUP_TIME()
+{
+      if [[ "$BASH_COMMAND" = "$PROMPT_COMMAND" ]]; then
+          return
+      fi
+    _COMMAND_START_TIME="$(date '+%s%3N' | tr -d '\n')"
 }
 
-PROMPT_COMMAND=_GEN_PROMPT
+trap _SAVE_STARTUP_TIME DEBUG
 
 stty susp undef
 stty -ixon
