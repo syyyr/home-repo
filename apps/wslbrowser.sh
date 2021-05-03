5b81672b31576b7419e763ffebc2e672a998706e
Václav Kubernát <sir.venceslas@gmail.com>
2021-05-03 12:39:08 +0200

    Fix wslbrowser.sh
    Powershell doesn't like ampersands, so let's wrap the argument in
    additional quotes.

--- apps/wslbrowser.sh
+++ apps/wslbrowser.sh
@@ -1,1 +1,2 @@
-/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe Start chrome.exe "$@"
+ADDRESS="$1"
+/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe Start chrome.exe \""$ADDRESS"\"
