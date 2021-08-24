a8eac721feaf4a1d983a334bdb7740e92098590d
Václav Kubernát <sir.venceslas@gmail.com>
2021-08-24 11:14:35 +0200

    dwdiff: Handle == output from doctest

--- .config/bashrc/functions.bash
+++ .config/bashrc/functions.bash
@@ -152,9 +152,15 @@ gso()
 
 string_diff()
 {
-    "$HOME/apps/check_available.bash"  dwdiff || return 1
+    "$HOME/apps/check_available.bash" dwdiff || return 1
 
-    dwdiff -c <(echo "$1") <(echo "$2")
+    if [[ "$*" =~ "==" ]]; then
+        local LEFT=$(grep ".* == " -o <<< "$*" | sed 's/ == //')
+        local RIGHT=$(grep " == .*" -o <<< "$*" | sed 's/ == //')
+        dwdiff -c <(echo "$LEFT") <(echo "$RIGHT")
+    else
+        dwdiff -c <(echo "$1") <(echo "$2")
+    fi
 }
 
 twitch()
