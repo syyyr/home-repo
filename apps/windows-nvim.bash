1ed0ae290230005e9a144eddea7e08c42e3fdba3
Václav Kubernát <sir.venceslas@gmail.com>
2021-09-12 19:27:39 +0200

    Fix windows-nvim

--- apps/windows-nvim.bash
+++ apps/windows-nvim.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 set +H
+export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
 echo "$@"
 WIN_PATH=$(wslpath "$@")
 echo "$WIN_PATH"
-xfce4-terminal --hide-menubar --hide-scrollbar --hide-toolbar --display=localhost:0 -e "nvim '$WIN_PATH'"
+xfce4-terminal --hide-menubar --hide-scrollbar --hide-toolbar -e "nvim '$WIN_PATH'"
