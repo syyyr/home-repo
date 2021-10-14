d3ede5aa8bff45b91e9d68fb04fdd1c80cd598af
Václav Kubernát <sir.venceslas@gmail.com>
2021-10-14 10:55:11 +0200

    Change DAYSTART to 8 am
    :)

--- .config/bashrc/env.bash
+++ .config/bashrc/env.bash
@@ -1,4 +1,4 @@
-export DAYSTART='9'
+export DAYSTART='8'
 export EDITOR='nvim'
 export FZF_DEFAULT_COMMAND='find . -type d \( -name ".cargo" -or -name ".local" -or -name ".rustup" -or -name ".npm" -or -name ".git" -or -name ".cache" \) -prune -o -type f -print'
 export LESS='-RFS'
--- .profile
+++ .profile
@@ -1,4 +1,4 @@
-export DAYSTART="9"
+export DAYSTART="8"
 export NIGHTSTART="22"
 export PATH="/home/vk/bin:$PATH"
 export PATH="/home/vk/.cargo/bin:$PATH"
