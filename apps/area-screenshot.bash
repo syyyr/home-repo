29f739c8d194aaa69936ae100933afb13b4b7a93
Václav Kubernát <sir.venceslas@gmail.com>
2021-12-02 17:53:19 +0100

    Switch from escrotum to scrot
    Scrot is now maintained again. It now has a lot more features, so the
    area-screenshot.bash script is not needed anymore. Great!

--- apps/area-screenshot.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-"$HOME/apps/check_available.bash" escrotum feh || exit 1
-
-escrotum /tmp/screenshot > /dev/null
-feh /tmp/screenshot &
-escrotum -s -C
-killall feh
-rm /tmp/screenshot
-"$HOME/apps/rakupscreen.bash"
