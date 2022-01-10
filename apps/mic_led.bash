57624ff1d73415b1c2fdb6923f0bf02f10818670
Václav Kubernát <sir.venceslas@gmail.com>
2022-01-11 00:59:54 +0100

    Fix mic muting
    We're now targeting the name instead of the id.

--- apps/mic_led.bash
+++ apps/mic_led.bash
@@ -2,7 +2,7 @@
 
 LED_FILE='/sys/class/leds/platform::micmute/brightness'
 
-if pamixer --get-mute --source 1; then
+if pamixer --get-mute --source 'alsa_input.pci-0000_06_00.6.HiFi__hw_acp__source'; then
     echo 1 > "$LED_FILE"
 else
     echo 0 > "$LED_FILE"
