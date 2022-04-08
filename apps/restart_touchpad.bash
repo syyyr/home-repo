84ed8fcac7b8a91a21d2cd0b10244dbbf900f182
Václav Kubernát <sir.venceslas@gmail.com>
2022-04-08 12:46:10 +0200

    restart_touchpad: Changes for T14s

--- apps/restart_touchpad.bash
+++ apps/restart_touchpad.bash
@@ -1,8 +1,3 @@
 #!/bin/bash
-sudo rmmod psmouse
-sudo modprobe psmouse
-sleep 1
-xinput set-prop 'Synaptics TM2668-002' 'libinput Natural Scrolling Enabled' 1
-xinput set-prop 'Synaptics TM2668-002' 'libinput Tapping Enabled' 1
-xinput set-prop 'SynPS/2 Synaptics TouchPad' 'libinput Natural Scrolling Enabled' 1
-xinput set-prop 'SynPS/2 Synaptics TouchPad' 'libinput Tapping Enabled' 1
+sudo rmmod thinkpad_acpi
+sudo modprobe thinkpad_acpi
