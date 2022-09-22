aa95a4bc0e174e04d0d8f46cfb1926ce74382a46
Václav Kubernát <sir.venceslas@gmail.com>
2022-09-22 23:41:14 +0200

    Improve rakdl
    I donẗ remember what exactly this does, but it probably works.

--- apps/rakdl.bash
+++ apps/rakdl.bash
@@ -1,8 +1,7 @@
 #!/bin/bash
 "$HOME/apps/check-available.bash" fzf
 
-# Set TERM and use dircolors to tell remote bash that I'm using a color enabled terminal
-FILE=$(ssh "rak@anip.icu" 'export TERM=xterm-256color; eval $(dircolors); ls -A --color=always www/rakac' | fzf -0 --height=50% --border --ansi);
+FILE=$(ssh -t "rak@anip.icu" 'ls -1A --color=always www/rakac' | fzf -0 --height=50% --border --ansi | tr -d '')
 if [[ "$?" -ne 0 ]]; then
     exit 1
 fi
