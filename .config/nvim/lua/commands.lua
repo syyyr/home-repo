3cca4222d749f6d0c56f92cd306f15c9f359074b
Václav Kubernát <sir.venceslas@gmail.com>
2022-10-27 17:19:10 +0200

    Remove some unused stuff

--- .config/nvim/lua/commands.lua
+++ .config/nvim/lua/commands.lua
@@ -1,10 +1,8 @@
 vim.api.nvim_create_user_command('Trailing', function() vim.cmd('call custom#CleanExtraSpaces()') end, { nargs = 0 })
 vim.api.nvim_set_keymap('n', '<A-t>', ':Trailing<cr>', {noremap = true})
 
---" quick diff
-vim.g.diff = 0
-vim.api.nvim_create_user_command('DiffToggle', Custom.diff_toggle, { nargs = 0 })
 --" toggle unsaved changes diff
+vim.api.nvim_create_user_command('DiffToggle', Custom.diff_toggle, { nargs = 0 })
 vim.api.nvim_set_keymap('n', '<A-,>', '', {noremap = true, callback = Custom.diff_toggle })
 
 --" end with :Q XD
