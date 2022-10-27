eefe23a72c2e746ca67e40771468db899084f9e5
Václav Kubernát <sir.venceslas@gmail.com>
2022-10-27 10:42:12 +0200

    lsp: sumneko_lua: Recognize nvim api

--- .config/nvim/plugins.lua
+++ .config/nvim/plugins.lua
@@ -183,7 +183,17 @@ require("lspconfig").bashls.setup({
 
 require("lspconfig").sumneko_lua.setup({
     on_attach = on_attach,
-    capabilities = capabilities
+    capabilities = capabilities,
+    settings = {
+	Lua = {
+	    diagnostics = {
+		globals = {'vim'},
+	    },
+	    workspace = {
+		library = vim.api.nvim_get_runtime_file("", true),
+	    },
+	}
+    }
 })
 
 require("inc_rename").setup({
