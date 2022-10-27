b0405e7eb1b21e4d866c3dbaa4ca36f8ea000e19
Václav Kubernát <sir.venceslas@gmail.com>
2022-10-27 14:43:54 +0200

    Complete migration to Lua
    Apart from some ftplugin stuff, we're done here.

---
+++ .config/nvim/init.lua
@@ -0,0 +1,13 @@
+require('skeleton')
+-- custom commands, and their associated mappings
+require('commands')
+-- plugins and their options
+require('plugins')
+-- simple vim options "set smth="
+require('options')
+-- advanced vim options
+require('startup')
+-- simple mappings
+require('mappings')
+-- syntax highlighting and various color changes
+require('colors')
