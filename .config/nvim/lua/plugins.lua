3b67b929484dbd20c13687c3aa6b605f40de33a1
Václav Kubernát <sir.venceslas@gmail.com>
2022-11-09 10:21:59 +0100

    Add CQ/CQA commands

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -65,6 +65,9 @@ vim.g.dispatch_no_maps = 1
 
 vim.api.nvim_create_user_command('CF', function() vim.lsp.buf.code_action() end, {nargs = 0})
 vim.api.nvim_create_user_command('CRef', function() vim.lsp.buf.references() end, {nargs = 0})
+vim.api.nvim_create_user_command('CQ', function () vim.diagnostic.setloclist({severity = vim.diagnostic.severity.ERROR}) end, {nargs = 0})
+vim.api.nvim_create_user_command('CQA', vim.diagnostic.setloclist, {nargs = 0})
+
 vim.api.nvim_create_autocmd('CursorHold', {
     callback = function () vim.diagnostic.open_float(nil, {focus = false, scope = 'cursor'}) end,
     group = vim.api.nvim_create_augroup('LSPHover', {clear = true})
