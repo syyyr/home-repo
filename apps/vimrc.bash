ccb6706b9d888f5556f655d7939b2f87788915ce
Václav Kubernát <sir.venceslas@gmail.com>
2022-11-17 13:57:17 +0100

    vimrc: Fix "all files" mode

--- apps/vimrc.bash
+++ apps/vimrc.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ $# -eq 0 ]]; then
-    nvim -p "$HOME/.config/nvim/"*".vim" "$HOME/.config/nvim/autoload/custom.vim"
+    nvim -p "$HOME/.config/nvim/init.lua" "$HOME/.config/nvim/lua/"*
 else
     read -ra ARGS < <(printf "$HOME/.config/nvim/%s " "$@")
     nvim -p "${ARGS[@]}"
