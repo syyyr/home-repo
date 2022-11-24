ff5e5fb4064a11618758869605718490d9e80889
Václav Kubernát <sir.venceslas@gmail.com>
2022-11-24 16:12:27 +0100

    lsp: Ignore Lua empty-block

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -268,6 +268,7 @@ require('lspconfig').sumneko_lua.setup({
             },
             diagnostics = {
                 globals = {'vim'},
+                disable = {'empty-block'}
             },
             workspace = {
                 library = vim.api.nvim_get_runtime_file('', true),
