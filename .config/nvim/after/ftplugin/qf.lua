4d608554db08f313c1867e9ab03ec9fee8e2fc86
Václav Kubernát <sir.venceslas@gmail.com>
2022-11-24 17:15:33 +0100

    Add comments to Quickfix statusline

--- .config/nvim/after/ftplugin/qf.lua
+++ .config/nvim/after/ftplugin/qf.lua
@@ -1,1 +1,11 @@
-vim.wo.statusline=[[%=%=%20{exists('w:quickfix_title')? ' '.w:quickfix_title : '[no command]'}%r %-30.(ln %l col %c%)%=%#StatusLineNC#Quickfix%=]]
+vim.wo.statusline =
+    '%=%=' .. -- Separators.
+    [[%20{exists('w:quickfix_title')? ' '.w:quickfix_title : '[no command]'}]] .. -- Show Quickfix title
+    '%r' .. -- Show [RO] sign.
+    ' ' .. -- A literal space.
+    '%-30.' .. -- Add minimal width.
+    '(ln %l col %c%)' .. -- Show line number and column number.
+    '%=' .. -- Separator.
+    '%#StatusLineNC#' .. -- Set highlight group
+    'Quickfix' .. -- Literal "Quickfix", as a filetype.
+    '%='-- Separator.
