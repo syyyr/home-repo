c1108d93b2b7442666ac31a084a6237c85779cdb
Václav Kubernát <sir.venceslas@gmail.com>
2023-01-11 19:05:37 +0100

    Don't restore position for gitcommit and gitrebase

--- .config/nvim/lua/startup.lua
+++ .config/nvim/lua/startup.lua
@@ -10,6 +10,9 @@ end
 
 vim.api.nvim_create_autocmd('BufReadPost', {
     callback = function()
+        if vim.o.filetype == 'gitcommit' or vim.o.filetype == 'gitrebase' then
+            return
+        end
         local mark = vim.api.nvim_buf_get_mark(0, '"')
         local lcount = vim.api.nvim_buf_line_count(0)
         if mark[1] > 0 and mark[1] <= lcount then
