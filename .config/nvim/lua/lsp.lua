1dff52865f292bec62f6cc18f0e98fa8f111b728
Václav Kubernát <sir.venceslas@gmail.com>
2023-01-19 15:28:40 +0100

    CQA: Use quicklist

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -170,7 +170,7 @@ require('nvim-custom-diagnostic-highlight').setup({})
 vim.api.nvim_create_user_command('CF', function() vim.lsp.buf.code_action({apply = true}) end, {nargs = 0})
 vim.api.nvim_create_user_command('Cref', vim.lsp.buf.references, {nargs = 0})
 vim.api.nvim_create_user_command('CQ', function() vim.diagnostic.setloclist({severity = vim.diagnostic.severity.ERROR}) end, {nargs = 0})
-vim.api.nvim_create_user_command('CQA', vim.diagnostic.setloclist, {nargs = 0})
+vim.api.nvim_create_user_command('CQA', vim.diagnostic.setqflist, {nargs = 0})
 
 vim.lsp.handlers["textDocument/hover"] = function(err, result, ctx, config)
     local _
