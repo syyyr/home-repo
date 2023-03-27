0830a7b508a2a08f29a3e70bbf94daa3376239a2
Václav Kubernát <sir.venceslas@gmail.com>
2023-03-27 18:26:36 +0200

    Use [[ instead of [

--- .config/bashrc/env.bash
+++ .config/bashrc/env.bash
@@ -8,7 +8,7 @@ export MANPAGER='nvim +Man!'
 export NIGHTSTART='22'
 export NPM_CONFIG_USERCONFIG="$HOME/.config/npm/npmrc"
 OUTPUT=$(xrandr | grep -o '^.* connected' | grep -v 'eDP-1' | sed 's/ connected//')
-if [ -n "$OUTPUT" ]; then
+if [[ -n "$OUTPUT" ]]; then
 	export QT_SCALE_FACTOR=1.5
 fi
 unset OUTPUT
