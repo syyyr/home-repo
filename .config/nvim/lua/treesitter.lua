9d50ac3ebe702c3eab45504b952037609ee1b741
Václav Kubernát <sir.venceslas@gmail.com>
2023-05-13 09:47:30 +0200

    cpon: Switch back to upstream treesitter

--- .config/nvim/lua/treesitter.lua
+++ .config/nvim/lua/treesitter.lua
@@ -1,15 +1,6 @@
 vim.cmd('packadd! nvim-treesitter')
 vim.cmd('packadd! nvim-treesitter-playground')
 
-require('nvim-treesitter.parsers').get_parser_configs().cpon = {
-    install_info = {
-        url = '/home/vk/git/tree-sitter-cpon', -- local path or git repo
-        files = {'src/parser.c'},
-        generate_requires_npm = false,
-        requires_generate_from_grammar = true,
-    }
-}
-
 require('nvim-treesitter.configs').setup({
     highlight = {
         enable = true,
@@ -26,6 +17,7 @@ require('nvim-treesitter.configs').setup({
     ensure_installed = {
         'bash',
         'comment',
+        'cpon',
         'cpp',
         'lua',
         'python',
