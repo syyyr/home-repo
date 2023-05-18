e220b731d5602172dcbab524cf86f7bc61694d4f
Václav Kubernát <sir.venceslas@gmail.com>
2023-05-18 15:52:42 +0200

    c.vim: Add printing function
    Can only print integers, but that's fine - I can just change the format
    letter, and I don't want to change the commands just because of C.

--- .config/nvim/ftplugin/c.lua
+++ .config/nvim/ftplugin/c.lua
@@ -1,3 +1,24 @@
-require('custom').nnoremap('é', '<cmd>ClangdSwitchSourceHeader<cr>', {buffer = true})
+local function add_stdio()
+    if vim.fn.search('stdio.h', 'nw') == 0 then
+        vim.fn.append(0, '#include <stdio.h>')
+    end
+end
 
+-- TODO: refactor this function
+local function escape_double_quotes(text)
+    return text:gsub([["]], [[\"]])
+end
+
+require('custom').nnoremap('é', '<cmd>ClangdSwitchSourceHeader<cr>', {buffer = true})
 vim.o.commentstring='//%s'
+
+require('custom').register_printing({
+    print_var = function(var_name)
+        add_stdio()
+        return [[fprintf(stderr, "%s = %d\n", "]] .. escape_double_quotes(var_name) .. [[", ]] .. var_name .. [[);]]
+    end,
+    print_text = function(text)
+        add_stdio()
+        return [[fprintf(stderr, "%s\n", ]] .. escape_double_quotes(text) .. [[);]]
+    end,
+})
