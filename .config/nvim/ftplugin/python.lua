362b58ab55f8fab6c6cd03fd3db01f06de2083da
Václav Kubernát <sir.venceslas@gmail.com>
2023-06-08 16:36:57 +0200

    python: Add printing

--- .config/nvim/ftplugin/python.lua
+++ .config/nvim/ftplugin/python.lua
@@ -1,1 +1,14 @@
 vim.cmd('packadd vim-python-pep8-indent')
+
+local function escape_double_quotes(text)
+    return text:gsub([["]], [[\"]])
+end
+
+require('custom').register_printing({
+    print_var = function(var_name)
+        return 'print("' .. escape_double_quotes(var_name) .. '", "=", ' .. var_name .. ')'
+    end,
+    print_text = function(text)
+        return 'print("' .. escape_double_quotes(text) .. '")'
+    end,
+})
