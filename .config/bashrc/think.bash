48f3160c064cb889cff3e5fd9ad99dc71c1ce7e5
Václav Kubernát <sir.venceslas@gmail.com>
2023-06-13 15:47:06 +0200

    think: Do not add $HOME/bin
    It is in .profile.

--- .config/bashrc/think.bash
+++ .config/bashrc/think.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-export PATH="$HOME/bin:$PATH"
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="/opt/cesnet/bin:$PATH"
