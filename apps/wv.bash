85c333c406630509bddeb9129f0c4322831f46db
Václav Kubernát <sir.venceslas@gmail.com>
2023-06-26 15:37:51 +0200

    wv: Check for binary files

--- apps/wv.bash
+++ apps/wv.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
+FILE_PATH="$(command -v "$1")"
+if ! grep --binary-files=without-match --regexp="" "$FILE_PATH" &> /dev/null; then
+	echo "$FILE_PATH is a binary file." >&2
+	exit 1
+fi
 
-${EDITOR:-vim} "$(command -v "$1")"
+${EDITOR:-vim} "$FILE_PATH"
