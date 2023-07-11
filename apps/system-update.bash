9e39a8b6da363ee21d0b3c02e2e578ea6db5e827
Václav Kubernát <sir.venceslas@gmail.com>
2023-07-11 10:42:02 +0200

    system-update: Run everything in $HOME

--- apps/system-update.bash
+++ apps/system-update.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
+pushd "$HOME"
 sudo pacman -Syu --noconfirm
 exec 3< <(git submodule update --remote |& cat)
 UPDATE_PID="$!"
@@ -9,7 +10,6 @@ auracle outdated || echo "No outdated AUR packages."
 echo "Waiting for submodules to finish updating..."
 wait "$UPDATE_PID"
 cat <&3
-pushd "$HOME"
 echo Updating tree-sitter parsers...
 nvim --headless -c TSUpdateSync -c q
 echo # The output from nvim doesn't have a trailing newline
