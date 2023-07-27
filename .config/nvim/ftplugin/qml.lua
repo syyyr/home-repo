1900f1925ed96818d9e263915b68c1b228799e4c
Václav Kubernát <sir.venceslas@gmail.com>
2023-07-27 22:18:33 +0200

    Add printing for qml

---
+++ .config/nvim/ftplugin/qml.lua
@@ -0,0 +1,10 @@
+local syyyr = require('syyyr')
+
+syyyr.register_printing({
+    print_var = function(var_name)
+        return 'console.log("' .. syyyr.escape_double_quotes(var_name) .. '", "=", ' .. var_name .. ')'
+    end,
+    print_text = function(text)
+        return 'console.log("' .. syyyr.escape_double_quotes(text) .. '")'
+    end,
+})
