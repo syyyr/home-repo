262950c837659bb2a0973fd745d369e37abf5faa
Václav Kubernát <sir.venceslas@gmail.com>
2023-07-31 10:06:57 +0200

    backtrace: Fix editor invocation

--- apps/backtrace.bash
+++ apps/backtrace.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 
-vim -c 'lua vim.o.errorformat =[[%\s%#%m%# %f:%l:%c,%f:%l:%c: %m]]' -c "cfile $1"
+"${EDITOR:-vim}" -c 'lua vim.o.errorformat =[[%\s%#%m%# %f:%l:%c,%f:%l:%c: %m]]' -c "cfile $1"
