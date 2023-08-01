0248b2919bc5f758bba7da75be4cde206e150167
Václav Kubernát <sir.venceslas@gmail.com>
2023-08-01 16:40:52 +0200

    brightness: Switch to brightnessctl
    light has been abandoned :/

--- apps/brightness.bash
+++ apps/brightness.bash
@@ -1,31 +1,29 @@
 #!/bin/bash
-"$HOME/apps/check-available.bash" light python || exit 1
+"$HOME/apps/check-available.bash" brightnessctl python || exit 1
 
 case "$1" in
-    # The rates at which changes happen aren't the same, the increasing is slower. I don't mind however, as increasing is
-    # potentially more irritating to the eye.
     increase)
-        light -T 1.5
+        brightnessctl --exponent set +10%
         echo "Raising brightness."
         ;;
     decrease)
-        light -T 0.7
+        brightnessctl --exponent set 10%-
         echo "Lowering brightness."
         ;;
     max)
-        light -S 100
+        brightnessctl --exponent set 255
         ;;
     min)
-        light -S 1 # using 1, otherwise T440s just turns off the screen completely
+        brightnessctl --exponent set 0
         ;;
     *)
         ;;
 esac
 
-BRIGHT="$(light | python -c "print(round(float(input())))")" # Python rounds the output
+BRIGHT="$(brightnessctl --exponent --machine-readable info | cut -d',' -f 4)"
 
-echo "Brightness: $BRIGHT"%
+echo "Brightness: $BRIGHT"
 
 BRIGHTNESS_IMG="$HOME/.local/share/icons/blue/brightness.png"
 ARGS=( -t 2000 -h string:x-canonical-private-synchronous:brightness -i "$BRIGHTNESS_IMG" )
-notify-send "${ARGS[@]}" 'Brightness' "$BRIGHT"%
+notify-send "${ARGS[@]}" 'Brightness' "$BRIGHT"
