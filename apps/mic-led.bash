2275244d127de68620e842aca40ee29b05b68ec4
Václav Kubernát <sir.venceslas@gmail.com>
2023-08-01 16:20:38 +0200

    micmute: Use default source

--- apps/mic-led.bash
+++ apps/mic-led.bash
@@ -2,7 +2,7 @@
 
 LED_FILE='/sys/class/leds/platform::micmute/brightness'
 
-if [[ "$(pamixer --get-mute --source 'alsa_input.pci-0000_05_00.6.HiFi__hw_acp__source')" = "true" ]]; then
+if [[ "$(pamixer --get-mute --default-source)" = "true" ]]; then
     echo 1 > "$LED_FILE"
 else
     echo 0 > "$LED_FILE"
