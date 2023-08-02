6fd75f2b7226455d2fe41a433a862200928a6ef9
Václav Kubernát <sir.venceslas@gmail.com>
2023-08-02 18:33:51 +0200

    update-aur-dep: Flatten code flow

--- apps/update-aur-dep.bash
+++ apps/update-aur-dep.bash
@@ -6,24 +6,33 @@ readonly BASH_COLOR_RED=$'\033''[31m'
 readonly BASH_COLOR_NORMAL=$'\033''[0m'
 
 readonly UPDATE_COMMAND=(env MAKEFLAGS="-j$(nproc)" makepkg -si --noconfirm --needed)
+readonly AUR_DEP="$1"
 
-pushd "$HOME/.local/aur/$1"
-git reset --hard
-if ! "${UPDATE_COMMAND[@]}"; then
-	# Try to remove the src directory if the build fails.
-	echo "${BASH_COLOR_BOLD}${BASH_COLOR_RED}Building $1 failed.${BASH_COLOR_NORMAL}"
-	echo "Removing $HOME/.local/aur/$1/src and trying again..."
-	rm -rf "$HOME/.local/aur/$1/src"
-	if ! "${UPDATE_COMMAND[@]}"; then
-		echo "${BASH_COLOR_BOLD}${BASH_COLOR_RED}Building $1 failed.${BASH_COLOR_NORMAL}"
-		echo -n 'Try with --skippgpcheck? [Y/n] '
-		read -r
-		if [[ -n "$REPLY" ]] && ! [[ "$REPLY" =~ [Yy] ]]; then
-			echo 'Aborting.'
-			exit 1
-		fi
-		echo "Trying again with --skippgpcheck..."
-		"${UPDATE_COMMAND[@]}" --skippgpcheck
+try_again_message() {
+	echo "${BASH_COLOR_BOLD}${BASH_COLOR_RED}Building $AUR_DEP failed.${BASH_COLOR_NORMAL}"
+	echo -n "Try again with ${1}? [Y/n] "
+	read -r
+	if [[ -n "$REPLY" ]] && ! [[ "$REPLY" =~ [Yy] ]]; then
+		echo 'Aborting.'
+		exit 1
 	fi
+	echo "Trying again with ${1}..."
+}
+
+pushd "$HOME/.local/aur/$AUR_DEP"
+git reset --hard
+
+if "${UPDATE_COMMAND[@]}"; then
+	exit 0
 fi
+
+try_again_message '--skippgpcheck'
+if "${UPDATE_COMMAND[@]}" --skippgpcheck; then
+	exit 0
+fi
+
+try_again_message 'removing the build directory'
+echo "Removing $HOME/.local/aur/$AUR_DEP/src and trying again..."
+rm -rf "$HOME/.local/aur/$AUR_DEP/src"
+"${UPDATE_COMMAND[@]}"
 popd
