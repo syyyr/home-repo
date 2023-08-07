75503f215a387f0c6bcc47d7ff85a2d9493be3d0
Václav Kubernát <sir.venceslas@gmail.com>
2023-08-07 10:44:42 +0200

    copypasta: Add ab

--- apps/copypasta.bash
+++ apps/copypasta.bash
@@ -61,6 +61,7 @@ tm ™
 green_circle 🟢
 sushi 🍣
 linux/interject I'\''d just like to interject for a moment. What you'\''re referring to as Linux, is in fact, GNU/Linux, or as I'\''ve recently taken to calling it, GNU plus Linux. Linux is not an operating system unto itself, but rather another free component of a fully functioning GNU system made useful by the GNU corelibs, shell utilities and vital system components comprising a full OS as defined by POSIX.
+ab 🆎
 b 🅱️'
 
 SELECTED_PASTA="$(rofi -dmenu -p "> " -sort -matching fuzzy -scroll-method 1 <<< "$PASTAS")"
