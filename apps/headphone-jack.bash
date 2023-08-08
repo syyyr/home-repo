4b6809606e3ebc6de8565bdef310f4636714f45a
Václav Kubernát <sir.venceslas@gmail.com>
2023-08-08 10:44:13 +0200

    Add headphone-jack

---
+++ apps/headphone-jack.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+refresh() {
+    py3-cmd refresh volume_status
+}
+
+TO_MATCH=(
+    -e "jack/microphone MICROPHONE unplug"
+    -e "jack/headphone HEADPHONE unplug"
+    -e "jack/headphone HEADPHONE plug"
+    -e "jack/microphone MICROPHONE plug"
+)
+
+acpi_listen | while IFS= read -r line; do
+    if ! grep -qF "${TO_MATCH[@]}" <<< "$line"; then
+        continue
+    fi
+
+    refresh
+done
