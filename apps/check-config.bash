3759eb8bd25a1c7fb5ff40ee1dc78e2bd69e63d2
Václav Kubernát <sir.venceslas@gmail.com>
2023-08-15 13:56:33 +0200

    check-config: Add lua-language-server check

--- apps/check-config.bash
+++ apps/check-config.bash
@@ -2,3 +2,8 @@
 
 luacheck -q "$HOME/.config/nvim" | grep -v 'Total:'
 shellcheck -a "$HOME"/.config/bashrc/*
+LOGFILE="$(lua-language-server --check "$HOME/.config/nvim" --checklevel=Hint | grep -oE "[^ ]+check\.json")"
+if [[ -n "$LOGFILE" ]]; then
+	jq -r 'to_entries | map((.key | sub("file://"; "")) as $key | .value | map($key + ":" + (.range.start.line + 1 | tostring) + ":" + (.range.start.character + 1 | tostring) + ": " + .message + " [" + .code + "]") | .[]) | .[]' "$LOGFILE"
+	rm "$LOGFILE"
+fi
