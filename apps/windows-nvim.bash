e0799795c55d2f48c718e7df6ef059a93f60ad52
Václav Kubernát <sir.venceslas@gmail.com>
2023-09-03 18:35:11 +0200

    windows-nvim: Use kitty

--- apps/windows-nvim.bash
+++ apps/windows-nvim.bash
@@ -3,4 +3,4 @@ set +H
 echo "$@"
 WIN_PATH=$(wslpath "$@")
 echo "$WIN_PATH"
-xfce4-terminal --hide-menubar --hide-scrollbar --hide-toolbar -e "nvim '$WIN_PATH'"
+"$HOME/apps/kitty.wrapper" nvim "$WIN_PATH"
