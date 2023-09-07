799196784a80fc57775ec2d6336580a2452263e5
Václav Kubernát <sir.venceslas@gmail.com>
2023-09-07 22:44:08 +0200

    cpon: Add pattern

--- .config/nvim/ftdetect/cpon.lua
+++ .config/nvim/ftdetect/cpon.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'}, {
-    pattern = '*.cpon',
+    pattern = '*.cpon,eyassrv*.conf',
     callback = function() vim.bo.filetype = 'cpon' end,
     group = vim.api.nvim_create_augroup('CponDetection', {clear = true})
 })
