ebd68610708512504f238a1a0bce39a5d76ecf58
Václav Kubernát <sir.venceslas@gmail.com>
2023-09-11 09:50:32 +0200

    Rewrite update-aur-dep

--- apps/update-aur-dep.bash
+++ apps/update-aur-dep.bash
@@ -4,40 +4,52 @@ set -u
 readonly BASH_COLOR_BOLD=$'\033''[1m'
 readonly BASH_COLOR_RED=$'\033''[31m'
 readonly BASH_COLOR_NORMAL=$'\033''[0m'
-
-readonly UPDATE_COMMAND=(env CTEST_PARALLEL_LEVEL="$(nproc)" MAKEFLAGS="-j$(nproc)" makepkg -si --noconfirm --needed)
+readonly BASH_BLUE_BOLD=$'\033''[01;34m'
 readonly AUR_DEP="$1"
 
-try_again_message() {
-	echo "${BASH_COLOR_BOLD}${BASH_COLOR_RED}Building $AUR_DEP failed.${BASH_COLOR_NORMAL}"
-	echo -n "Try again with ${1}? [Y/n] "
-	read -r
-	if [[ -n "$REPLY" ]] && ! [[ "$REPLY" =~ [Yy] ]]; then
-		echo 'Aborting.'
-		return 1
+info() {
+	if [[ "$1" = "-n" ]]; then
+		local ARGS=("-n")
+		local TEXT="$2"
+	else
+		local ARGS=()
+		local TEXT="$1"
 	fi
-	echo "Trying again with ${1}..."
+	ARGS+=("${BASH_BLUE_BOLD}$TEXT${BASH_COLOR_NORMAL}")
+	echo "${ARGS[@]}"
 }
+
+UPDATE_COMMAND=(env CTEST_PARALLEL_LEVEL="$(nproc)" MAKEFLAGS="-j$(nproc)" makepkg -si --noconfirm --needed)
 
 pushd "$HOME/.local/aur/$AUR_DEP" || exit 1
 git reset --hard
 
-if "${UPDATE_COMMAND[@]}"; then
-	exit 0
-fi
-
-if try_again_message '--nocheck'; then
-	"${UPDATE_COMMAND[@]}" --nocheck && exit 0
-fi
-
-if try_again_message '--skippgpcheck'; then
-	"${UPDATE_COMMAND[@]}" --skippgpcheck && exit 0
-fi
-
-if try_again_message 'removing the build directory'; then
-	echo "Removing $HOME/.local/aur/$AUR_DEP/src and trying again..."
-	rm -rf "$HOME/.local/aur/$AUR_DEP/src"
+while true; do
+	info "Building $AUR_DEP..."
 	"${UPDATE_COMMAND[@]}" && exit 0
-fi
+
+	echo "Building $AUR_DEP failed."
+	info -n "Try again [(n)ocheck,(s)kippgpcheck,(r)emove_build_dir]? "
+	read -r
+
+	if [[ -z "$REPLY" ]]; then
+		break
+	fi
+
+	if [[ "$REPLY" =~ "n" ]]; then
+		echo "Adding --nocheck...";
+		UPDATE_COMMAND+=(--nocheck)
+	fi
+
+	if [[ "$REPLY" =~ "s" ]]; then
+		echo "Adding --skippgpcheck...";
+		UPDATE_COMMAND+=(--skippgpcheck)
+	fi
+
+	if [[ "$REPLY" =~ "r" ]]; then
+		echo "Removing $HOME/.local/aur/$AUR_DEP/src and trying again..."
+		rm -rf "$HOME/.local/aur/$AUR_DEP/src"
+	fi
+done
 
 echo "${BASH_COLOR_BOLD}${BASH_COLOR_RED}Building $AUR_DEP failed. Giving up.${BASH_COLOR_NORMAL}"
