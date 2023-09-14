1626572cf5bff01c9909bc8a0ac5a94b0d967601
Václav Kubernát <sir.venceslas@gmail.com>
2023-09-14 09:52:47 +0200

    cpp: Add Sleep

--- .config/nvim/ftplugin/cpp.lua
+++ .config/nvim/ftplugin/cpp.lua
@@ -4,6 +4,12 @@ local function add_iostream()
     end
 end
 
+local function add_thread()
+    if vim.fn.search('<thread>', 'nw') == 0 then
+        vim.fn.append(0, '#include <thread>')
+    end
+end
+
 local function have_qt()
     return vim.fn.search('#include <Q', 'nw') ~= 0
 end
@@ -27,6 +33,11 @@ syyyr.register_printing({
         return get_stream() .. [[ << "]] .. syyyr.escape_double_quotes(text) .. (have_qt() and [[";]] or [[\n";]])
     end,
 })
+
+vim.api.nvim_buf_create_user_command(0, 'Sleep', function(info)
+    add_thread()
+    vim.cmd.normal({'ostd::this_thread::sleep_for(std::chrono::seconds(' .. info.args .. '));', bang = true})
+end, { nargs = 1 })
 
 vim.cmd([[iabbrev <buffer> DOCSUB DOCTEST_SUBCASE("")<Left><Left>]])
 vim.o.cinoptions='j1,(0,ws,Ws,:0,l1,N-s,E-s'
