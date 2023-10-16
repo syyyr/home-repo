20cbd28c8ce6a9c11d4d1d861686004cb0f1160d
Václav Kubernát <sir.venceslas@gmail.com>
2023-10-16 15:53:20 +0200

    cpp: Fix Qt detection once again

--- .config/nvim/ftplugin/cpp.lua
+++ .config/nvim/ftplugin/cpp.lua
@@ -14,7 +14,7 @@ local syyyr = require('syyyr')
 
 local function have_qt()
     return syyyr.any({'#include <Q', 'QString', 'QDateTime'}, function(needle)
-        return vim.fn.search(needle, 'nw')
+        return vim.fn.search(needle, 'nw') ~= 0
     end)
 end
 
