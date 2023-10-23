6299f6f8d0e0f1a81a758df9bc3f095dd448e8d1
Václav Kubernát <sir.venceslas@gmail.com>
2023-10-23 15:25:42 +0200

    Use vim.opt
    Cause why not, I'm bored.

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,51 +1,51 @@
-vim.o.title = true -- set title to NVIM
-vim.o.showmode = false
-vim.o.cursorline = true
+vim.opt.title = true -- set title to NVIM
+vim.opt.showmode = false
+vim.opt.cursorline = true
 
 vim.cmd('syntax enable') -- enable syntax highlighting
-vim.o.shiftwidth = 4 -- << >> 4 spaces
+vim.opt.shiftwidth = 4 -- << >> 4 spaces
 
 -- no esc delay
-vim.o.timeout = true
-vim.o.ttimeout = true
-vim.o.timeoutlen = 1000
-vim.o.ttimeoutlen = 10
+vim.opt.timeout = true
+vim.opt.ttimeout = true
+vim.opt.timeoutlen = 1000
+vim.opt.ttimeoutlen = 10
 
 -- set 7 lines to the cursor
-vim.o.scrolloff = 7
+vim.opt.scrolloff = 7
 
 -- sane opening new files
-vim.o.hidden = true
+vim.opt.hidden = true
 
 -- performance hack
-vim.o.lazyredraw = true
+vim.opt.lazyredraw = true
 
 -- ignore case when searching, but only for lowercase letters
-vim.o.ignorecase = true
-vim.o.smartcase = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 -- matching brackets
-vim.o.showmatch = true
+vim.opt.showmatch = true
 vim.opt.matchpairs:append('<:>,«:»')
 
 -- incremental commands
-vim.o.inccommand = 'split'
+vim.opt.inccommand = 'split'
 
 -- always show fold column, good for the margin
-vim.o.foldcolumn = '1'
+vim.opt.foldcolumn = '1'
 
 -- better linebreak
-vim.o.linebreak = true
+vim.opt.linebreak = true
 
 -- disable swap file
-vim.o.swapfile = false
+vim.opt.swapfile = false
 
 -- better ex command autocomplete
-vim.o.wildmode = 'list:longest'
+vim.opt.wildmode = 'list:longest'
 
 -- undo
-vim.o.undofile = true
-vim.o.undolevels = 5000
+vim.opt.undofile = true
+vim.opt.undolevels = 5000
 
 -- set path for :find
 vim.o.path='.'
@@ -57,24 +57,24 @@ vim.opt.path:append('public/**')
 vim.opt.path:append('include/**')
 
 -- pretty windows split :>
-vim.o.fillchars = 'fold: ,vert:▏'
+vim.opt.fillchars = 'fold: ,vert:▏'
 
 -- windows are vertically split the other way
-vim.o.splitright = true
+vim.opt.splitright = true
 
 -- system clipboard is used along with the unnamed register
-vim.o.clipboard = 'unnamedplus'
+vim.opt.clipboard = 'unnamedplus'
 
 -- don't scan tagfiles
 vim.opt.complete:remove('t')
 
 -- don't autoload file if it's changed outside of vim
-vim.o.autoread = false
+vim.opt.autoread = false
 
 -- the time for CursorHold trigger
-vim.o.updatetime=300
+vim.opt.updatetime=300
 
-vim.o.statusline =
+vim.opt.statusline =
     '%#StatusLineNC#' .. -- Set highlight group.
     '%-20.' ..  -- Add padding to the diagnostic to prevent flickering when the diagnostic appears and disappears.
     [[{v:lua.require('syyyr').statusline_diagnostics()}]] .. -- Show diagnostics.
@@ -101,16 +101,16 @@ vim.opt.shortmess:append('I')
 
 vim.opt.diffopt:append('algorithm:patience')
 
-vim.o.signcolumn = 'yes'
+vim.opt.signcolumn = 'yes'
 
 -- Don't wrap text when it's longer than 'textwidth'
 vim.opt.formatoptions:remove('t')
-vim.o.textwidth = 120
+vim.opt.textwidth = 120
 
-vim.o.makeprg = 'make -C build'
+vim.opt.makeprg = 'make -C build'
 
-vim.o.commentstring = '# %s'
+vim.opt.commentstring = '# %s'
 
-vim.o.mouse = ''
+vim.opt.mouse = ''
 
-vim.o.tabstop = 4
+vim.opt.tabstop = 4
