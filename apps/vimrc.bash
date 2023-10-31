cc345dd58c2bcb55da0fa31daf22cf99e49652c9
Václav Kubernát <sir.venceslas@gmail.com>
2023-10-31 15:38:03 +0100

    vimrc: Fix default files

--- apps/vimrc.bash
+++ apps/vimrc.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [[ $# -eq 0 ]]; then
-    nvim -p "$HOME/.config/nvim/init.lua" "$HOME/.config/nvim/lua/"*
+    readarray -t FILES < <(find "$HOME/.config/nvim/init.lua" "$HOME/.config/nvim/lua/"* -type f)
+    nvim -p "${FILES[@]}"
 else
     read -ra ARGS < <(printf "$HOME/.config/nvim/%s " "$@")
     nvim -p "${ARGS[@]}"
