5b528b2bf99e82f1e67010fb1304a38c9636e6f9
Václav Kubernát <sir.venceslas@gmail.com>
2023-11-02 11:33:29 +0100

    think: Remove obsolete PATH elements

--- .config/bashrc/think.bash
+++ .config/bashrc/think.bash
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 export PATH="$HOME/.local/bin:$PATH"
-export PATH="/opt/cesnet/bin:$PATH"
-export PATH="/opt/cesnet-au/bin:$PATH"
-export PATH="/opt/cesnet-t/bin:$PATH"
 
 source "$HOME/.config/bashrc/common.bash"
