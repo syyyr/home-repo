1afb1deab042681de92312a353a83ba3ee825098
Václav Kubernát <sir.venceslas@gmail.com>
2023-12-04 14:14:30 +0100

    Merge do-auracle-update to system-update

--- apps/system-update.bash
+++ apps/system-update.bash
@@ -1,12 +1,26 @@
 #!/bin/bash
 set -euxo pipefail
 
+filter_disabled_packages() {
+    local DISABLED_PKGS=(
+        # These are part of another package.
+        -e "mingw-w64-harfbuzz-icu"
+    )
+    grep -v "${DISABLED_PKGS[@]}"
+}
+
 pushd "$HOME"
 sudo pacman -Syu --noconfirm
 exec 3< <(git submodule update --remote |& cat)
 UPDATE_PID="$!"
 auracle outdated || echo "No outdated AUR packages."
-"$HOME/apps/do-auracle-update.bash"
+
+pushd "$HOME/.local/aur"
+for i in $(auracle -q outdated | filter_disabled_packages) $(pacman -Qqs '.-git$' | filter_disabled_packages); do
+    "$HOME/apps/update-aur-dep.bash" "$i" || true
+done
+popd
+
 echo "Waiting for submodules to finish updating..."
 wait "$UPDATE_PID"
 cat <&3
