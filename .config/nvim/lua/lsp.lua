5944b5dfedb024fea9459ce3ce1ee06a3a15cb48
Václav Kubernát <sir.venceslas@gmail.com>
2023-12-11 10:36:45 +0100

    lsp: Improve capabilities setting
    From nvim-cmp.

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -58,7 +58,10 @@ cmp.setup({
     },
 })
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local capabilities = vim.tbl_deep_extend("force",
+    vim.lsp.protocol.make_client_capabilities(),
+    require('cmp_nvim_lsp').default_capabilities()
+)
 capabilities.offsetEncoding = {'utf-16'}
 
 vim.cmd('packadd! nvim-lspconfig')
