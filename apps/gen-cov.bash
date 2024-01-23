a0b0a1d3ddbfafdab5d5056840875c7869b3a631
Václav Kubernát <sir.venceslas@gmail.com>
2024-01-23 11:32:56 +0100

    gen-cov: Improvements
    1) First argument is the binary.
    2) Rest of the arguments are sources to show coverage from.
    3) Use demangler.

--- apps/gen-cov.bash
+++ apps/gen-cov.bash
@@ -2,6 +2,19 @@
 set -euo pipefail
 shopt -s inherit_errexit
 
-llvm-profdata merge -sparse default.profraw -o default.profdata
-llvm-cov show --instr-profile=default.profdata "$1" --format=html --ignore-filename-regex='3rdparty|autogen'
+if [[ "${1+x}" != x ]]; then
+	echo You have to specify an executable / a library as the first argument. >&2
+	exit 1
+fi
 
+BINARY="$1"
+shift
+
+EXTRA_ARGS=("$@")
+
+if [[ "${2+x}" = x ]]; then
+	EXTRA_ARGS+=("$2")
+fi
+
+llvm-profdata merge -sparse default.profraw -o default.profdata
+llvm-cov show --instr-profile=default.profdata "$BINARY" --format=html --Xdemangler=c++filt --ignore-filename-regex='3rdparty|autogen' "${EXTRA_ARGS[@]}"
