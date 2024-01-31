0006b804721f4794e12cda4ca197fdacacd37ea4
Václav Kubernát <sir.venceslas@gmail.com>
2024-01-31 21:08:16 +0100

    update-aur-dep: Remove command logging

--- apps/update-aur-dep.bash
+++ apps/update-aur-dep.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euxo pipefail
+set -euo pipefail
 shopt -s inherit_errexit
 
 readonly BASH_COLOR_BOLD=$'\033''[1m' BASH_COLOR_RED=$'\033''[31m' BASH_BLUE_BOLD=$'\033''[01;34m' BASH_COLOR_NORMAL=$'\033''[0m' AUR_DEP="$1"
