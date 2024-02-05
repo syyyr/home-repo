785a87565b9391aec5ac2dd169cff5c226426911
Václav Kubernát <sir.venceslas@gmail.com>
2024-02-05 11:25:51 +0100

    Ignore last position for git files

--- .config/nvim/lua/startup.lua
+++ .config/nvim/lua/startup.lua
@@ -9,9 +9,6 @@ end
 
 vim.api.nvim_create_autocmd('BufReadPost', {
     callback = function()
-        if vim.opt.filetype == 'gitcommit' or vim.opt.filetype == 'gitrebase' then
-            return
-        end
         local mark = vim.api.nvim_buf_get_mark(0, '"')
         local lcount = vim.api.nvim_buf_line_count(0)
         if mark[1] > 0 and mark[1] <= lcount then
@@ -19,6 +16,14 @@ vim.api.nvim_create_autocmd('BufReadPost', {
         end
     end,
     group = vim.api.nvim_create_augroup('LastPostion', {clear = true})
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'gitcommit,gitrebase',
+    callback = function()
+        vim.api.nvim_win_set_cursor(0, {1, 0})
+    end,
+    group = vim.api.nvim_create_augroup('LastPostionGit', {clear = true})
 })
 
 local statusline_augroup = vim.api.nvim_create_augroup('StatuslineIntegration', {clear = true})
