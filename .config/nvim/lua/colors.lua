b6ecce1901c3d93e06d9710a2f8bc05aed2bb74d
Václav Kubernát <sir.venceslas@gmail.com>
2024-02-08 15:43:07 +0100

    Refactor highlighting

--- .config/nvim/lua/colors.lua
+++ .config/nvim/lua/colors.lua
@@ -22,87 +22,81 @@ vim.api.nvim_create_autocmd('ColorScheme', {
 
         vim.cmd('highlight StatusLineNC ' .. separator_colors .. ' cterm=underline gui=underline')
         vim.cmd('highlight StatusLine ' .. separator_colors .. ' cterm=underline,bold gui=underline,bold')
-
         vim.cmd('highlight WinSeparator ' .. separator_colors)
 
         vim.cmd('highlight search ctermbg=11 guifg=#444444 guibg=#ffffaa')
-        vim.cmd('highlight! link CurSearch Search')
-
         vim.cmd('highlight clear EndOfBuffer')
-        vim.cmd('highlight! link EndOfBuffer NonText')
-
         vim.cmd('highlight MergeConflict ctermbg=black ctermfg=red guibg=black guifg=red')
-
         vim.cmd('highlight Todo ctermbg=NONE guibg=NONE')
-
-        vim.cmd('highlight! link LowerCaseNote Todo')
-
-        vim.cmd('highlight! link ErrorMsg LspDiagnosticsDefaultError')
-
         vim.cmd('highlight! Conceal guibg=NONE')
 
-        vim.cmd('highlight! link NormalFloat Pmenu')
+        vim.cmd('highlight DiagnosticOk gui=bold guifg=#629120')
+
         vim.cmd('highlight clear LspDiagnosticsDefaultError')
         vim.cmd('highlight LspDiagnosticsDefaultError ctermfg=124 guifg=#af0000')
-        vim.cmd('highlight! link DiagnosticFloatingError LspDiagnosticsDefaultError')
 
-        vim.cmd('highlight LspDiagnosticsDefaultWarning guibg=NONE')
         if vim.o.background == 'dark' then
             vim.cmd('highlight LspDiagnosticsDefaultWarning guifg=#00af5f')
             vim.cmd('highlight LspDiagnosticsUnderlineWarning guisp=#00af5f')
             vim.cmd('highlight Todo guifg=#00af5f')
         end
 
-        for _, group in ipairs({
-            'DiagnosticFloatingHint',
-            'DiagnosticFloatingInfo',
-            'DiagnosticFloatingWarn',
-            'LspDiagnosticsDefaultHint',
-            'LspDiagnosticsDefaultInformation'}) do
-            vim.cmd('highlight! link ' .. group .. ' LspDiagnosticsDefaultWarning ')
+        vim.cmd('highlight LspDiagnosticsDefaultWarning guibg=NONE')
+        for syntax_group, highlight_group in pairs({
+            ['CurSearch'] = 'Search',
+            ['EndOfBuffer'] = 'NonText',
+            ['LowerCaseNote'] = 'Todo',
+            ['ErrorMsg'] = 'LspDiagnosticsDefaultError',
+            ['NormalFloat'] = 'Pmenu',
+            ['DiagnosticFloatingError'] = 'LspDiagnosticsDefaultError',
+
+            ['DiagnosticFloatingHint'] = 'LspDiagnosticsDefaultWarning',
+            ['DiagnosticFloatingInfo'] = 'LspDiagnosticsDefaultWarning',
+            ['DiagnosticFloatingWarn'] = 'LspDiagnosticsDefaultWarning',
+            ['LspDiagnosticsDefaultHint'] = 'LspDiagnosticsDefaultWarning',
+            ['LspDiagnosticsDefaultInformation'] = 'LspDiagnosticsDefaultWarning',
+
+            ['@class'] = 'NONE',
+            ['@comment'] = 'Comment',
+            ['@enum'] = 'Constant',
+            ['@keyword'] = 'Keyword',
+            ['@keyword.conditional'] = 'Conditional',
+            ['@keyword.import'] = 'Include',
+            ['@keyword.repeat'] = 'Repeat',
+            ['@macro'] = 'Macro',
+            ['@module'] = 'NONE',
+            ['@namespace'] = 'NONE',
+            ['@parameter'] = 'NONE',
+            ['@preproc'] = 'NONE',
+            ['@property'] = 'NONE',
+            ['@punctuation.bracket'] = 'NONE',
+            ['@punctuation.special'] = 'Keyword',
+            ['@punctuation.delimiter'] = 'NONE',
+            ['@type'] = 'NONE',
+            ['@type.builtin'] = 'Type',
+            ['@typeParameter'] = 'Constant',
+            ['@variable'] = 'NONE',
+
+            ['@text.uri.comment'] = 'NONE',
+
+            ['@lsp.type.class'] = 'NONE',
+            ['@lsp.type.comment.lua'] = 'NONE',
+            ['@lsp.type.enum'] = 'Constant',
+            ['@lsp.type.function'] = 'NONE',
+            ['@lsp.type.macro'] = 'Macro',
+            ['@lsp.type.namespace'] = 'NONE',
+            ['@lsp.type.parameter'] = 'NONE',
+            ['@lsp.type.property'] = 'NONE',
+            ['@lsp.type.typeParameter'] = 'Constant',
+            ['@lsp.type.variable'] = 'NONE',
+
+            ['@keyword.return.cpp'] = 'Statement',
+            ['@lsp.mod.deduced.cpp'] = 'Type',
+            ['@lsp.type.typeParameter.cpp'] = 'NONE',
+            ['@type.qualifier.cpp'] = 'cStorageClass',
+        }) do
+            vim.cmd(('highlight! link %s %s'):format(syntax_group, highlight_group))
         end
-
-        -- Treesitter
-        vim.cmd('highlight! link @class NONE')
-        vim.cmd('highlight! link @comment Comment')
-        vim.cmd('highlight! link @enum Constant')
-        vim.cmd('highlight! link @keyword Keyword')
-        vim.cmd('highlight! link @keyword.conditional Conditional')
-        vim.cmd('highlight! link @keyword.import Include')
-        vim.cmd('highlight! link @keyword.repeat Repeat')
-        vim.cmd('highlight! link @macro Macro')
-        vim.cmd('highlight! link @module NONE')
-        vim.cmd('highlight! link @namespace NONE')
-        vim.cmd('highlight! link @parameter NONE')
-        vim.cmd('highlight! link @preproc NONE')
-        vim.cmd('highlight! link @property NONE')
-        vim.cmd('highlight! link @punctuation.bracket NONE')
-        vim.cmd('highlight! link @punctuation.special Keyword')
-        vim.cmd('highlight! link @punctuation.delimiter NONE')
-        vim.cmd('highlight! link @type NONE')
-        vim.cmd('highlight! link @type.builtin Type')
-        vim.cmd('highlight! link @typeParameter Constant')
-        vim.cmd('highlight! link @variable NONE')
-        vim.cmd('highlight! link @variable NONE')
-
-        vim.cmd('highlight! link @lsp.type.class NONE')
-        vim.cmd('highlight! link @lsp.type.comment.lua NONE')
-        vim.cmd('highlight! link @lsp.type.enum Constant')
-        vim.cmd('highlight! link @lsp.type.function NONE')
-        vim.cmd('highlight! link @lsp.type.macro Macro')
-        vim.cmd('highlight! link @lsp.type.namespace NONE')
-        vim.cmd('highlight! link @lsp.type.parameter NONE')
-        vim.cmd('highlight! link @lsp.type.property NONE')
-        vim.cmd('highlight! link @lsp.type.typeParameter Constant')
-        vim.cmd('highlight! link @lsp.type.variable NONE')
-        vim.cmd('highlight! link @text.uri.comment NONE')
-
-        vim.cmd('highlight! link @type.qualifier.cpp cStorageClass')
-        vim.cmd('highlight! link @lsp.mod.deduced.cpp Type')
-        vim.cmd('highlight! link @lsp.type.typeParameter.cpp NONE')
-        vim.cmd('highlight! link @keyword.return.cpp Statement')
-
-        vim.cmd('highlight DiagnosticOk gui=bold guifg=#629120')
     end,
     group = vim.api.nvim_create_augroup('PaperColorOverride', {clear = true})
 })
