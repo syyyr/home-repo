385d7a17725467e8edfd43ba83edf7b3d438f87d
Václav Kubernát <sir.venceslas@gmail.com>
2024-02-15 11:56:04 +0100

    Reorder aliases

--- .config/bashrc/alias.bash
+++ .config/bashrc/alias.bash
@@ -7,12 +7,13 @@ alias arch-clone="cd $HOME/git/arch-packages && pkgctl repo clone --protocol htt
 alias aur-srcinfo='makepkg --printsrcinfo > .SRCINFO'
 alias calc="python -q"
 # shellcheck disable=SC2139
+alias {d,dc,scd}='cd'
+# shellcheck disable=SC2139
 alias {cd..,dc..}='cd ..'
 alias cdp='cd -P'
 alias cpu='cat /proc/cpuinfo | grep "MHz"'
 alias ctest='ctest --output-on-failure'
 # shellcheck disable=SC2139
-alias {d,dc,scd}='cd'
 alias dicker='docker'
 alias drive="rclone mount gdrive: mnt"
 alias explorer='taskkill -f explorer.exe; cmd "start /B explorer"& exit'
@@ -20,16 +21,16 @@ alias ffplay='ffplay -autoexit'
 alias free='free -h'
 alias get_screen='xclip -se c -o -target image/png'
 alias grep='grep --color=auto'
-alias i3config="nvim \$HOME/.config/i3/config"
 alias i3config-status="nvim \$HOME/.config/i3status/config"
+alias i3config="nvim \$HOME/.config/i3/config"
 alias icat="kitty +kitten icat"
 alias ivm="nvim"
+alias krc="nvim \$HOME/.config/kitty/custom.conf"
 alias l='ls -CF'
 alias la='ls -A'
 alias lah='ls -lah'
 alias ll='ls -alF'
 alias ls='ls --color=auto --hyperlink=auto'
-alias krc="nvim \$HOME/.config/kitty/custom.conf"
 alias makec='make clean'
 alias rsync='rsync --progress'
 alias se='sudoedit'
@@ -37,6 +38,8 @@ alias sl='ls'
 alias sr='screen -r'
 alias ssh='kitten ssh'
 alias vim='nvim'
+alias watch='watch -n1'
+alias x86_64-w64-mingw32-wine='LC_ALL=C x86_64-w64-mingw32-wine'
 alias 🤔="cat \$HOME/apps/thinking"
 for i in {1..64}; do
     # shellcheck disable=SC2139
@@ -44,6 +47,7 @@ for i in {1..64}; do
 done; unset i;
 alias makej="make -j\$(nproc)"
 alias maekj="makej"
+alias makje="makej"
 
 alias adog='git adog'
 alias c='git clone --recurse-submodules'
@@ -51,6 +55,4 @@ alias pull='git pull'
 alias push='git push'
 alias reflog='git reflog'
 alias show='git show'
-alias x86_64-w64-mingw32-wine='LC_ALL=C x86_64-w64-mingw32-wine'
-alias watch='watch -n1'
 complete -F _complete_alias "${!BASH_ALIASES[@]}"
