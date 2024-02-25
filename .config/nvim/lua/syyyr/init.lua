8342e3080fac7559399ecf9c95cc11b21ee59722
Václav Kubernát <sir.venceslas@gmail.com>
2024-02-25 23:08:01 +0100

    Use constants for severities

--- .config/nvim/lua/syyyr/init.lua
+++ .config/nvim/lua/syyyr/init.lua
@@ -17,10 +17,10 @@ end
 
 ---@return string
 function M.statusline_diagnostics()
-    for _, severity in pairs({"ERROR", "WARN", "INFO", "HINT"}) do
-        local diagnostic = vim.diagnostic.get(0, {severity = vim.diagnostic.severity[severity]})[1]
+    for _, severity in pairs({vim.diagnostic.severity.ERROR, vim.diagnostic.severity.WARN, vim.diagnostic.severity.INFO, vim.diagnostic.severity.HINT}) do
+        local diagnostic = vim.diagnostic.get(0, {severity})[1]
         if diagnostic then
-            return format_statusline_diagnostics(severity:sub(1, 1), tostring(diagnostic.lnum + 1))
+            return format_statusline_diagnostics(vim.diagnostic.severity[severity]:sub(1, 1), tostring(diagnostic.lnum + 1))
         end
     end
     if vim.b.TrailingNr and vim.b.TrailingNr ~= '' and vim.api.nvim_get_mode() ~= 'i' then
