ca842686029ce68a6e026dbbc8749f220435ff11
Václav Kubernát <sir.venceslas@gmail.com>
2024-02-26 12:12:18 +0100

    check-config: Print commands run

--- apps/check-config.bash
+++ apps/check-config.bash
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-selene --no-summary --config .config/nvim/selene.toml .config/nvim/
-shellcheck -a "$HOME"/.config/bashrc/*
-LOGFILE="$(lua-language-server --check "$HOME/.config/nvim" --checklevel=Hint | grep -oE "[^ ]+check\.json")"
+print_and_run()
+{
+	echo RUN: "$@" >&2
+	"$@"
+}
+
+print_and_run selene --no-summary --config .config/nvim/selene.toml .config/nvim/
+print_and_run shellcheck -a "$HOME"/.config/bashrc/*
+LOGFILE="$(print_and_run lua-language-server --check "$HOME/.config/nvim" --checklevel=Hint | grep -oE "[^ ]+check\.json")"
 if [[ -n "$LOGFILE" ]]; then
 	jq -r 'to_entries | map((.key | sub("file://"; "")) as $key | .value | map($key + ":" + (.range.start.line + 1 | tostring) + ":" + (.range.start.character + 1 | tostring) + ": " + .message + " [" + .code + "]") | .[]) | .[]' "$LOGFILE"
 	rm "$LOGFILE"
