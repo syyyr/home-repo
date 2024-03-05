1fb2c0f7c1d3eef1ff34ed840e18294f3bc32b43
Václav Kubernát <sir.venceslas@gmail.com>
2024-03-05 10:10:18 +0100

    Add ibus

---
+++ apps/ibus-restarter.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+while true; do
+	echo Wating for i3 socket...
+	while ! [[ -r "$(i3 --get-socketpath)" ]]; do
+		sleep 0.1
+	done
+	echo Socket ready.
+	echo Listening to i3.
+	i3-msg -t subscribe
+	echo Restarting ibus...
+	systemctl --user restart "ibus@:0.service"
+	echo Restarted ibus.
+done
+
