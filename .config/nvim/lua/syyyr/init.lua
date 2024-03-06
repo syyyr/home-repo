afa63189d8fc1b05a842d89d12b4fc3ebe811320
Václav Kubernát <sir.venceslas@gmail.com>
2024-03-06 17:17:18 +0100

    lsp: Fix statusline_diagnostics filtering

--- .config/nvim/lua/syyyr/init.lua
+++ .config/nvim/lua/syyyr/init.lua
@@ -18,7 +18,7 @@ end
 ---@return string
 function M.statusline_diagnostics()
     for _, severity in pairs({vim.diagnostic.severity.ERROR, vim.diagnostic.severity.WARN, vim.diagnostic.severity.INFO, vim.diagnostic.severity.HINT}) do
-        local diagnostic = vim.diagnostic.get(0, {severity})[1]
+        local diagnostic = vim.diagnostic.get(0, {severity = severity})[1]
         if diagnostic then
             return format_statusline_diagnostics(vim.diagnostic.severity[severity]:sub(1, 1), tostring(diagnostic.lnum + 1))
         end
