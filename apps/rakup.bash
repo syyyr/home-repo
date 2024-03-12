30c5657120e25e9434ddb65a3aae0eccbf0cda83
Václav Kubernát <sir.venceslas@gmail.com>
2024-03-12 14:05:23 +0100

    Rewrite rakup.bash

--- apps/rakup.bash
+++ apps/rakup.bash
@@ -1,27 +1,22 @@
 #!/bin/bash
+set -euo pipefail
+shopt -s inherit_errexit
 
-if [[ -z "$1" ]]; then
-    echo 'Specify filepath.'
-    exit 1
-fi
-
-url_encode() {
-    python -c "import furl, sys; print(furl.furl(sys.argv[1]).url)" "$1"
-}
-
-if [[ -z "$2" ]]; then
-    scp "$1" 'rak@anip.icu:www/rakac/'
-    url_encode "https://rakac.anip.icu/$(basename "$1")"
-    xclip -rmlastnl <<< "$(url_encode "https://rakac.anip.icu/$(basename "$1")")"
-else
+if [[ "$1" = "-" ]] || ! [[ -t 0 ]]; then
     if [[ "$1" = "-" ]]; then
-        cat | ssh "rak@anip.icu" "cat > www/rakac/$2"
-    else
-        scp "$1" "rak@anip.icu:www/rakac/$2"
+        shift
     fi
-
-    url_encode "https://rakac.anip.icu/$2"
-    xclip -rmlastnl <<< "$(url_encode "https://rakac.anip.icu/$2")"
+    TARGET_FILE="${1}"
+    cat | ssh "rak@anip.icu" "cat > www/rakac/$TARGET_FILE"
+else
+    if [[ $# = 1 ]]; then
+        TARGET_FILE="${1}"
+    else
+        TARGET_FILE="${2}"
+    fi
+    scp "$1" "rak@anip.icu:www/rakac/$TARGET_FILE"
 fi
 
-
+URL="$(python -c "import furl, sys; print(furl.furl(sys.argv[1]).url)" "https://rakac.anip.icu/$TARGET_FILE")"
+echo "$URL"
+xclip -rmlastnl <<< "$URL"
