5efb3de92657d3be222f3b74507ed63527e3ed6e
Václav Kubernát <sir.venceslas@gmail.com>
2024-05-02 18:39:28 +0200

    lock: Lock the screen as soon as possible

--- apps/lock.bash
+++ apps/lock.bash
@@ -1,5 +1,38 @@
 #!/bin/bash
-"$HOME/apps/check-available.bash" i3lock dunstctl xrandr xset || exit 1
+
+if xrandr | grep primary | grep -F "1920x1080" > /dev/null; then
+    TIME_SIZE=130
+    DATE_SIZE=59
+    TIME_POS=80:913
+    DATE_POS=80:ty+80
+else
+    TIME_SIZE=260
+    DATE_SIZE=118
+    TIME_POS=160:1826
+    DATE_POS=160:ty+160
+fi
+
+IMAGE_FILE="$HOME/.local/share/win10-lock-4k.png"
+i3lock \
+    --nofork \
+    --fill \
+    --clock \
+    --image="$IMAGE_FILE" \
+    --color=000000 \
+    --time-font=Segoeui \
+    --date-font=Segoeui \
+    --date-color=FFFFFF \
+    --time-color=FFFFFF \
+    --date-align=1 \
+    --time-align=1 \
+    --time-str="%H:%M" \
+    --date-str="%A, %d. %B" \
+    --time-size="$TIME_SIZE" \
+    --date-size="$DATE_SIZE" \
+    --time-pos="$TIME_POS" \
+    --date-pos="$DATE_POS" &
+
+i3-msg bar mode invisible
 
 get_muted()
 {
@@ -26,41 +59,8 @@ if [[ "$1" != "no-off" ]]; then
     SCREENOFF_PID="$!"
 fi
 
-if xrandr | grep primary | grep -F "1920x1080" > /dev/null; then
-    TIME_SIZE=130
-    DATE_SIZE=59
-    TIME_POS=80:913
-    DATE_POS=80:ty+80
-else
-    TIME_SIZE=260
-    DATE_SIZE=118
-    TIME_POS=160:1826
-    DATE_POS=160:ty+160
-fi
-
-IMAGE_FILE="$HOME/.local/share/win10-lock-4k.png"
-
 dunstctl set-paused true
-i3-msg bar mode invisible
-i3lock \
-    --nofork \
-    --fill \
-    --clock \
-    --image="$IMAGE_FILE" \
-    --color=000000 \
-    --time-font=Segoeui \
-    --date-font=Segoeui \
-    --date-color=FFFFFF \
-    --time-color=FFFFFF \
-    --date-align=1 \
-    --time-align=1 \
-    --time-str="%H:%M" \
-    --date-str="%A, %d. %B" \
-    --time-size="$TIME_SIZE" \
-    --date-size="$DATE_SIZE" \
-    --time-pos="$TIME_POS" \
-    --date-pos="$DATE_POS"
-
+wait
 dunstctl set-paused false
 i3-msg bar mode dock
 
