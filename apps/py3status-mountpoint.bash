3f6d350aca76f1b5d297186ddc29adb28594bc1e
Václav Kubernát <sir.venceslas@gmail.com>
2024-06-19 16:56:49 +0200

    py3status-mountpoint: Add smb mounts

--- apps/py3status-mountpoint.bash
+++ apps/py3status-mountpoint.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
 if mount -l -t fuse.rclone | grep gdrive &> /dev/null; then
-	echo "<span color='deepskyblue'>drive</span>"
+	echo -n "<span color='deepskyblue'>drive</span> "
 fi
+
+for MOUNTPOINT_PATH in $(mount | grep cifs | cut -d' ' -f1); do
+	echo -n "<span color='lime'>smb</span> $MOUNTPOINT_PATH "
+done; unset mountpoint
