b8953963ebb4d18001f55954b5f7e04f424dc7ff
Václav Kubernát <sir.venceslas@gmail.com>
2024-06-20 18:01:48 +0200

    rust: Add printing

--- .config/nvim/ftplugin/rust.lua
+++ .config/nvim/ftplugin/rust.lua
@@ -1,1 +1,12 @@
 vim.cmd('packadd rust.vim')
+
+local syyyr = require('syyyr')
+
+require('syyyr').register_printing({
+    print_var = function(var_name)
+        return ([[eprintln!("%s = '{:?}'", %s);]]):format(syyyr.escape_double_quotes(var_name), var_name)
+    end,
+    print_text = function(text)
+        return ([[eprintln!("%s");]]):format(syyyr.escape_double_quotes(text))
+    end,
+})
