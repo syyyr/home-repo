a44677b5dcb8caaf209754f469020b0477eebd4f
Václav Kubernát <sir.venceslas@gmail.com>
2024-07-08 22:50:51 +0200

    Fix color in dark mode

--- .config/nvim/lua/colors.lua
+++ .config/nvim/lua/colors.lua
@@ -2,7 +2,7 @@ vim.opt.termguicolors = true
 
 vim.g.PaperColor_Theme_Options = {
     theme = {
-        default = {
+        ["default.light"] = {
             transparent_background = 0,
             allow_bold = 1,
             allow_italic = 1,
