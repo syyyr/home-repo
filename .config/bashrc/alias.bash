1033c67fa4ae242ddb9f3e2811a82b4f384ca23d
Václav Kubernát <sir.venceslas@gmail.com>
2024-10-15 12:01:55 +0200

    Add br

--- .config/bashrc/alias.bash
+++ .config/bashrc/alias.bash
@@ -45,6 +45,9 @@ alias maekj="makej"
 alias makje="makej"
 
 alias adog='git adog'
+br() {
+    git branch --contains HEAD | sed  -r 's@[^/]*/(.*)\)$@\1@' | xargs git switch -c
+}
 alias c='git clone --recurse-submodules'
 alias down='git log --oneline origin/master~..HEAD 2>/dev/null || git log --oneline origin/main~..HEAD'
 alias fetch='git fetch'
