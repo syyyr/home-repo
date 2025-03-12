52e917cbb8e01979c5ac13260fa8293b373601ee
Václav Kubernát <sir.venceslas@gmail.com>
2025-03-12 11:19:50 +0100

    typescript: Escape double quotes

--- .config/nvim/ftplugin/typescript.lua
+++ .config/nvim/ftplugin/typescript.lua
@@ -1,6 +1,7 @@
+local syyyr = require('syyyr')
 require('syyyr').register_printing({
     print_var = function(var_name)
-        return 'console.log("' .. var_name .. '", "=", ' .. var_name .. ');'
+        return 'console.log("' .. syyyr.escape_double_quotes(var_name) .. '", "=", ' .. var_name .. ');'
     end,
     print_text = function(text)
         return 'console.log("' .. text .. '");'
