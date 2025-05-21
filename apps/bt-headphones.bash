2679a8aa65d72ee9b51a3a839f12516e2d0c4fc9
Václav Kubernát <sir.venceslas@gmail.com>
2025-05-21 11:41:05 +0200

    bt-headphones: Add statusbar refresh
    Also rename, as now it does more stuff.

--- apps/bt-headphones.bash
+++ apps/bt-headphones.bash
@@ -13,11 +13,16 @@ echo Listening to disconnects on "${BT_HEADPHONES_MAC@Q}".
 # one line. Should be stable enough.
 bluetoothctl --timeout=99999999 -- version |& stdbuf -i 0 -o 0 -e 0 ansi2txt | stdbuf -i 0 -o 0 -e 0 tr '\r' '\n'| while read -r LINE; do
 	echo "bluetoothctl: '${LINE}'"
-	if [[ "$LINE" != "[CHG] Device ${BT_HEADPHONES_MAC} Connected: no" ]]; then
-		continue
+	if [[ "$LINE" =~ \[CHG\]\ Device\ ${BT_HEADPHONES_MAC}\ Connected:\ (yes|no) ]]; then
+		CONNECTION_STATUS="${BASH_REMATCH[1]}"
+		echo "New connection status: ${CONNECTION_STATUS}"
+		if [[ "${CONNECTION_STATUS}" = no ]]; then
+			echo -n Stopping music...
+			playerctl stop &> /dev/null || true
+			echo " done."
+		fi
+		sleep 2
+		py3-cmd refresh 'external_script pixel-buds'
+		py3-cmd refresh bluetooth
 	fi
-
-	echo -n Stopping music...
-	playerctl stop
-	echo " done."
 done
