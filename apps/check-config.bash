a3753f88c82c8166a15242b34c99764951a685b0
Václav Kubernát <sir.venceslas@gmail.com>
2025-06-23 10:13:30 +0200

    check-config: Fix relative paths

--- apps/check-config.bash
+++ apps/check-config.bash
@@ -8,9 +8,9 @@ print_and_run()
 	"$@"
 }
 
-print_and_run selene --no-summary --config .config/nvim/selene.toml .config/nvim/
+print_and_run selene --no-summary --config "$HOME/.config/nvim/selene.toml" "$HOME/.config/nvim/"
 print_and_run shellcheck -a "$HOME"/.config/bashrc/*
-LOGFILE="$(print_and_run lua-language-server --check=/home/vk/.config/nvim --checklevel=Hint | grep -oE "[^ ]+check\.json")"
+LOGFILE="$(print_and_run lua-language-server --check="$HOME/.config/nvim" --checklevel=Hint | grep -oE "[^ ]+check\.json")"
 if [[ -n "$LOGFILE" ]]; then
 	jq -r 'to_entries | map((.key | sub("file://"; "")) as $key | .value | map($key + ":" + (.range.start.line + 1 | tostring) + ":" + (.range.start.character + 1 | tostring) + ": " + .message + " [" + .code + "]") | .[]) | .[]' "$LOGFILE"
 	rm "$LOGFILE"
