e2131a5368542563b3b2e7be4ae57a405552b9e1
Václav Kubernát <sir.venceslas@gmail.com>
2025-08-18 10:53:20 +0200

    Allow connecting earbuds via py3status

---
+++ apps/bt-pixelbuds.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+shopt -s inherit_errexit
+
+if bluetoothctl devices Connected | grep 'Pixel Buds Pro' &> /dev/null; then
+    bluetoothctl disconnect
+else
+    pbpctrl show battery
+fi
+
+py3-cmd refresh bluetooth
+py3-cmd refresh 'external_script pixel-buds'
