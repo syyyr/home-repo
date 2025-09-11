b9a8a0412fbefef138e61af195c54047d498d91b
Václav Kubernát <sir.venceslas@gmail.com>
2025-09-11 15:41:09 +0200

    Add select-sink.bash

---
+++ apps/select-sink.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -euo pipefail
+shopt -s inherit_errexit
+
+SINKS="$(pamixer --list-sinks | grep -v 'Sinks:')"
+
+SELECTED_SINK="$(rofi -dmenu -p "> " -sort -matching fuzzy -scroll-method 1 <<< "$SINKS")"
+SINK_NAME="$(tr -d '"' <<< "$SELECTED_SINK" | cut -d ' ' -f 2)"
+echo Setting "'$SINK_NAME'" as default string...
+pactl set-default-sink "$SINK_NAME"
