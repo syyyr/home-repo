40c63108193dd3f2b0c0591ffb3e2ab8345d21be
Václav Kubernát <sir.venceslas@gmail.com>
2025-11-10 19:51:33 +0100

    ibus-restarter: Always restart ibus at the beginning

--- apps/ibus-restarter.bash
+++ apps/ibus-restarter.bash
@@ -2,6 +2,18 @@
 set -euo pipefail
 shopt -s failglob inherit_errexit
 
+do_restart()
+{
+    echo Restarting ibus...
+    systemctl --user restart 'ibus@:0.service'
+    echo Restarted ibus.
+}
+
+while ! systemctl --user is-active 'ibus@:0.service'; do
+    do_restart
+done
+
+
 while true; do
     echo Wating for i3 socket...
     while ! [[ -r "$(i3 --get-socketpath)" ]]; do
@@ -9,9 +21,7 @@ while true; do
     done
     echo Socket ready.
     echo Listening to i3.
-    i3-msg -t subscribe
-    echo Restarting ibus...
-    systemctl --user restart "ibus@:0.service"
-    echo Restarted ibus.
+    i3-msg -t subscribe || true
+    do_restart
 done
 
