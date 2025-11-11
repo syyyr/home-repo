d4f0e06cfe2ac744dca45656d94d373adf6fe1ce
Václav Kubernát <sir.venceslas@gmail.com>
2025-11-11 10:49:15 +0100

    vue: Use console.log for printing

--- .config/nvim/ftplugin/vue.lua
+++ .config/nvim/ftplugin/vue.lua
@@ -1,7 +1,8 @@
 local syyyr = require('syyyr')
 syyyr.register_printing({
     print_var = function(var_name)
-        return string.format([[console.log(`%s = ${%s}`);]], var_name, var_name)
+        -- console.log is better at printing stuff than string interpolation.
+        return string.format([[console.log('%s', '=', %s);]], var_name, var_name)
     end,
     print_text = function(text)
         return string.format([[console.log(`%s`)]], text)
