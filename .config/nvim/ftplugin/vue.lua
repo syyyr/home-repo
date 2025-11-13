670d0f41e53e64f142512c87f180455e838235b3
Václav Kubernát <sir.venceslas@gmail.com>
2025-11-13 11:34:32 +0100

    typescript/vue: Escape single quotes

--- .config/nvim/ftplugin/vue.lua
+++ .config/nvim/ftplugin/vue.lua
@@ -2,7 +2,7 @@ local syyyr = require('syyyr')
 syyyr.register_printing({
     print_var = function(var_name)
         -- console.log is better at printing stuff than string interpolation.
-        return string.format([[console.log('%s', '=', %s);]], var_name, var_name)
+        return string.format([[console.log('%s', '=', %s);]], syyyr.escape_single_quotes(var_name), var_name)
     end,
     print_text = function(text)
         return string.format([[console.log(`%s`)]], text)
