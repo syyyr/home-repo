b9fed0c9612547fdc718871160e09223a752e961
Václav Kubernát <sir.venceslas@gmail.com>
2025-11-19 15:00:42 +0100

    vue: Link with typescript

--- .config/nvim/ftplugin/vue.lua
+++ .config/nvim/ftplugin/vue.lua
@@ -1,10 +1,1 @@
-local syyyr = require('syyyr')
-syyyr.register_printing({
-    print_var = function(var_name)
-        -- console.log is better at printing stuff than string interpolation.
-        return string.format([[console.log('%s', '=', %s);]], syyyr.escape_single_quotes(var_name), var_name)
-    end,
-    print_text = function(text)
-        return string.format([[console.log(`%s`)]], text)
-    end,
-})
+typescript.lua
