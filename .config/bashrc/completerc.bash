914e1161ab969550254d657bb0beb8a04613a9a2
Václav Kubernát <sir.venceslas@gmail.com>
2025-12-17 15:49:12 +0100

    completerc: Do not run cmake if not needed

--- .config/bashrc/completerc.bash
+++ .config/bashrc/completerc.bash
@@ -73,6 +73,9 @@ complete -W 'toggle manual timeout' kbacklight_ctl
 
 __my_cmake_compl() {
     __add_completion_words "$(grep -Eo '[-a-z]+)$' "$HOME/apps/my_cmake.bash" | sed 's/)//')"
+    if [[ "${#COMP_WORDS["${COMP_CWORD}"]}" = 1 && "${COMP_WORDS["${COMP_CWORD}"]}" != - ]] || [[ "${#COMP_WORDS["${COMP_CWORD}"]}" = 2 && "${COMP_WORDS["${COMP_CWORD}"]}" != -D ]]; then
+        return 0
+    fi
     local CMAKE_OUTPUT="$(cmake -L .. 2> /dev/null | sed -En 's/(.*):BOOL=/-D\1=/p')"
     __add_completion_words "$(sed -En 's/ON/OFF/p' <<< "$CMAKE_OUTPUT")"
     __add_completion_words "$(sed -En 's/OFF/ON/p' <<< "$CMAKE_OUTPUT")"
