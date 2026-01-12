f07cd2d962805fab2906c5c416a0107d152e3f9e
Václav Kubernát <sir.venceslas@gmail.com>
2026-01-12 11:39:39 +0100

    rust: Use dbg! to print stuff

--- .config/nvim/ftplugin/rust.lua
+++ .config/nvim/ftplugin/rust.lua
@@ -2,9 +2,9 @@ local syyyr = require('syyyr')
 
 require('syyyr').register_printing({
     print_var = function(var_name)
-        return ([[eprintln!("%s = '{:?}'", %s);]]):format(syyyr.escape_double_quotes(var_name), var_name)
+        return ([[dbg!(%s);]]):format(var_name)
     end,
     print_text = function(text)
-        return ([[eprintln!("%s");]]):format(syyyr.escape_double_quotes(text))
+        return ([[dbg!("%s");]]):format(syyyr.escape_double_quotes(text))
     end,
 })
