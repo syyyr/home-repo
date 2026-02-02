bb8dd2deb7fe59c481515fc87bd0a9c18bef35f9
Václav Kubernát <sir.venceslas@gmail.com>
2026-02-02 10:16:35 +0100

    rust: Print with ref

--- .config/nvim/ftplugin/rust.lua
+++ .config/nvim/ftplugin/rust.lua
@@ -2,7 +2,7 @@ local syyyr = require('syyyr')
 
 require('syyyr').register_printing({
     print_var = function(var_name)
-        return ([[dbg!(%s);]]):format(var_name)
+        return ([[dbg!(&%s);]]):format(var_name)
     end,
     print_text = function(text)
         return ([[dbg!("%s");]]):format(syyyr.escape_double_quotes(text))
