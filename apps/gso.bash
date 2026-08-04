83507cbaf0c46fbc20c197e398cba686d1871a19
Václav Kubernát <sir.venceslas@gmail.com>
2026-08-04 14:04:10 +0200

    gso: Remove empty line for no matches

--- apps/gso.bash
+++ apps/gso.bash
@@ -58,6 +58,10 @@ RG_CMD=( rg "${RG_CMD_ARGS[@]}" --with-filename --line-number --column --regexp 
 echo "${RG_CMD[@]@Q}"
 RESULTS="${ "${RG_CMD[@]}" |& cat || RG_ERR_CODE="$?"; }"
 if [[ -v RG_ERR_CODE ]]; then
+    if [[ "$RG_ERR_CODE" = 1 ]]; then
+        exit 1
+    fi
+
     if ! [[ "$RESULTS" =~ "No files were searched" ]]; then
         echo "$RESULTS" >&2
         exit 1
