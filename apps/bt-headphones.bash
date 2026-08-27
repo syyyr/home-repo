37735123d4c6676ea273399662955fb451c8b422
Václav Kubernát <sir.venceslas@gmail.com>
2026-08-27 14:57:31 +0200

    Remove log flood from bt-headphones

--- apps/bt-headphones.bash
+++ apps/bt-headphones.bash
@@ -12,7 +12,7 @@ echo Listening to disconnects on "${BT_HEADPHONES_MAC@Q}".
 # There is no 'noop' command for bluetoothctl and there's no way to run it to just get the events. `version` spits out
 # one line. Should be stable enough.
 bluetoothctl --timeout=99999999 -- version |& stdbuf -i 0 -o 0 -e 0 ansi2txt | stdbuf -i 0 -o 0 -e 0 tr '\r' '\n'| while read -r LINE; do
-    echo "bluetoothctl: '${LINE}'"
+    # echo "bluetoothctl: '${LINE}'"
     if [[ "$LINE" =~ \[CHG\]\ Device\ ${BT_HEADPHONES_MAC}\ Connected:\ (yes|no) ]]; then
         CONNECTION_STATUS="${BASH_REMATCH[1]}"
         echo "New connection status: ${CONNECTION_STATUS}"
