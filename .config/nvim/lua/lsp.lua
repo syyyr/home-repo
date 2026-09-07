458025dab96c9caa88ed5f88b70a38a3c2192987
Václav Kubernát <sir.venceslas@gmail.com>
2026-09-07 09:26:57 +0200

    Add goto/type/def keymaps

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -316,6 +316,10 @@ syyyr.nnoremap('<c-space>', function()
     vim.lsp.buf.hover()
 end)
 
+syyyr.nnoremap('<c-]>', vim.lsp.buf.definition)
+syyyr.nnoremap('<c-[>', vim.lsp.buf.type_definition)
+syyyr.nnoremap('<c-w><c-[>', '<c-w><c-v><c-[>')
+
 vim.api.nvim_create_autocmd({'CursorHold', 'DiagnosticChanged'}, {
     callback = function()
         if vim.v.exiting ~= vim.NIL then
